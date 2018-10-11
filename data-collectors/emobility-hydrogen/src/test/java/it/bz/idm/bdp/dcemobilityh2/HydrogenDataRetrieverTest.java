5c6c98ec471fbf86faeb4390a63f0d55d83cae0a
mbuc <marco.bucciarelli@edp-progetti.it>
2018-10-11 21:48:30 +0200

    Refined tests that check the conversion of station and plug data. Splitted in two specific tests

--- data-collectors/emobility-hydrogen/src/test/java/it/bz/idm/bdp/dcemobilityh2/HydrogenDataRetrieverTest.java
+++ data-collectors/emobility-hydrogen/src/test/java/it/bz/idm/bdp/dcemobilityh2/HydrogenDataRetrieverTest.java
@@ -19,9 +19,10 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
 import it.bz.idm.bdp.dcemobilityh2.dto.HydrogenDto;
-import it.bz.idm.bdp.dto.DataMapDto;
-import it.bz.idm.bdp.dto.RecordDtoImpl;
 import it.bz.idm.bdp.dto.StationList;
+import it.bz.idm.bdp.dto.emobility.EchargingPlugDto;
+import it.bz.idm.bdp.dto.emobility.EchargingStationDto;
+import it.bz.idm.bdp.dto.emobility.OutletDtoV2;
 
 @ContextConfiguration(locations = { "classpath:/META-INF/spring/applicationContext.xml" })
 public class HydrogenDataRetrieverTest extends AbstractJUnit4SpringContextTests {
@@ -36,6 +37,9 @@ public class HydrogenDataRetrieverTest extends AbstractJUnit4SpringContextTests 
 
     private static final String TEST_FILE_FETCH = "/test_data/test_data_fetch.xml";
     private static final String TEST_FILE_PUSH  = "/test_data/test_data_push.xml";
+
+    private static final String TEST_STATION_ID = "TEST_IDX";
+    private static final String TEST_PLUG_ID    = "TEST_IDX-1";
 
     public static final String DATA_FETCH = "FETCH";
     public static final String DATA_PUSH  = "PUSH";
@@ -60,7 +64,7 @@ public class HydrogenDataRetrieverTest extends AbstractJUnit4SpringContextTests 
     }
 
     @Test
-    public void testConvertData() {
+    public void testConvertStationData() {
 
         try {
             String responseString = getTestData(DATA_FETCH);
@@ -68,13 +72,96 @@ public class HydrogenDataRetrieverTest extends AbstractJUnit4SpringContextTests 
             List<HydrogenDto> data = reader.convertResponseToInternalDTO(responseString);
 
             StationList stations = pusher.mapStations2Bdp(data);
-            StationList plugs    = pusher.mapPlugs2Bdp(data);
-            DataMapDto<RecordDtoImpl> map = pusher.mapData(data);
-            DataMapDto<RecordDtoImpl> plugRec = pusher.mapPlugData2Bdp(data);
+//            StationList plugs    = pusher.mapPlugs2Bdp(data);
+//            DataMapDto<RecordDtoImpl> map = pusher.mapData(data);
+//            DataMapDto<RecordDtoImpl> plugRec = pusher.mapPlugData2Bdp(data);
 
-            //Test data contains 2 stations, check the DTOs are correctly converted
+            //Test data contains 2 stations
             assertEquals(2, stations.size());
+
+            //Check that station list contains a station with ID=TEST_IDX and that input data is converted correctly 
+            StringBuffer errs = new StringBuffer();
+            boolean stationFound = false;
+            for ( int i=0 ; !stationFound && i<stations.size() ; i++ ) {
+                EchargingStationDto station = (EchargingStationDto) stations.get(i);
+                String id = station.getId();
+                if ( TEST_STATION_ID.equals(id) ) {
+                    stationFound = true;
+                    checkEquals(TEST_STATION_ID   , station.getId()            , errs, "ID is INCORRECT");
+                    checkEquals("TEST_NAME"       , station.getName()          , errs, "NAME is INCORRECT");
+                    checkEquals(11D               , station.getLongitude()     , errs, "LONGITUDE is INCORRECT");
+                    checkEquals(46D               , station.getLatitude()      , errs, "LATITUDE is INCORRECT");
+                    checkEquals("TEST_CITY"       , station.getMunicipality()  , errs, "MUNICIPALITY is INCORRECT");
+                    checkEquals("TEST_HOSTNAME"   , station.getProvider()      , errs, "PROVIDER is INCORRECT");
+                    checkEquals("TEST_CITY"       , station.getCity()          , errs, "CITY is INCORRECT");
+                    checkEquals("AVAILABLE"       , station.getState()         , errs, "STATE is INCORRECT");
+                    checkEquals("TEST_COMMENTS"   , station.getAccessInfo()    , errs, "ACCESS_INFO is INCORRECT");
+                    checkEquals("IIT"             , station.getOrigin()        , errs, "ORIGIN is INCORRECT");
+                    checkEquals("EChargingStation", station.getStationType()   , errs, "STATION_TYPE is INCORRECT");
+                    checkEquals(Boolean.TRUE      , station.getReservable()    , errs, "RESERVABLE is INCORRECT");
+                    checkEquals("PUBLIC"          , station.getAccessType()    , errs, "ACCESS_TYPE is INCORRECT");
+                }
+            }
+            if ( !stationFound ) {
+                Assert.fail("No station found with id: " + TEST_STATION_ID);
+            }
+            if ( errs.length() > 0 ) {
+                Assert.fail("Station converter failure: " + errs);
+            }
+
+        } catch (Exception e) {
+            String msg = "Exception in testConvertData: " + e;
+            LOG.error(msg, e);
+            Assert.fail(msg);
+        }
+
+    }
+
+    @Test
+    public void testConvertPlugData() {
+
+        try {
+            String responseString = getTestData(DATA_FETCH);
+
+            List<HydrogenDto> data = reader.convertResponseToInternalDTO(responseString);
+
+            StationList plugs    = pusher.mapPlugs2Bdp(data);
+
+            //Test data contains 2 plugs
             assertEquals(2, plugs.size());
+
+            //Check that plug list contains a plug with ID=TEST_IDX-1 and that input data is converted correctly 
+            StringBuffer errs = new StringBuffer();
+            boolean plugFound = false;
+            for ( int i=0 ; !plugFound && i<plugs.size() ; i++ ) {
+                EchargingPlugDto plug = (EchargingPlugDto) plugs.get(i);
+                String id = plug.getId();
+                if ( TEST_PLUG_ID.equals(id) ) {
+                    plugFound = true;
+                    checkEquals(TEST_PLUG_ID      , plug.getId()            , errs, "ID is INCORRECT");
+                    checkEquals("TEST_NAME - Punto di rifornimento", plug.getName(), errs, "NAME is INCORRECT");
+                    checkEquals(11D               , plug.getLongitude()     , errs, "LONGITUDE is INCORRECT");
+                    checkEquals(46D               , plug.getLatitude()      , errs, "LATITUDE is INCORRECT");
+                    checkEquals(TEST_STATION_ID   , plug.getParentStation() , errs, "PARENT_STATION is INCORRECT");
+                    checkEquals("IIT"             , plug.getOrigin()        , errs, "ORIGIN is INCORRECT");
+                    checkEquals("EChargingPlug"   , plug.getStationType()   , errs, "PLUG_TYPE is INCORRECT");
+
+                    List<OutletDtoV2> outlets = plug.getOutlets();
+                    if ( outlets!=null && outlets.size()>0 ) {
+                        OutletDtoV2 outlet = outlets.get(0);
+                        checkEquals(TEST_PLUG_ID              , outlet.getId()            , errs, "OUTLET ID is INCORRECT");
+                        checkEquals("700 bar, small vehicles" , outlet.getOutletTypeCode(), errs, "OUTLET_TYPE_CODE is INCORRECT");
+                    } else {
+                        errs.append(" - Plug with EMPTY OUTLET LIST");
+                    }
+                }
+            }
+            if ( !plugFound ) {
+                Assert.fail("No plug found with id: " + TEST_PLUG_ID);
+            }
+            if ( errs.length() > 0 ) {
+                Assert.fail("Plug converter failure: " + errs);
+            }
 
         } catch (Exception e) {
             String msg = "Exception in testConvertData: " + e;
@@ -127,6 +214,28 @@ public class HydrogenDataRetrieverTest extends AbstractJUnit4SpringContextTests 
             } catch (Exception ex) {
                 LOG.error("EXCEPTION during close: "+ex);
             }
+        }
+    }
+
+    public static void checkEquals(Object o1, Object o2, StringBuffer sb, String errMsg) {
+        if ( o1 == o2 ) {
+            return;
+        } 
+        if ( o1!=null && o2==null ) {
+            sb.append(" - "+errMsg);
+            return;
+        }
+        if ( o1==null && o2!=null ) {
+            sb.append(" - "+errMsg);
+            return;
+        }
+        if ( o1.getClass() != o2.getClass() ) {
+            sb.append(" - "+errMsg);
+            return;
+        }
+        if ( !o1.equals(o2) ) {
+            sb.append(" - "+errMsg+" ("+o2+")");
+            return;
         }
     }
 
