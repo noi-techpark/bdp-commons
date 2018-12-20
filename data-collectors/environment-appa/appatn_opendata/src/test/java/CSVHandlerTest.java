f6ff37480083215f9b50ac85355166fa22a23947
Frank <francesco@datatellers.info>
2018-12-20 11:12:22 +0100

    Added APPA TN OpenData data collector, refactored old tenminutes folder into appatn_tenminutes (preparing for appabz)

---
+++ data-collectors/environment-appa/appatn_opendata/src/test/java/CSVHandlerTest.java
@@ -0,0 +1,57 @@
+import info.datatellers.appatn.helpers.CSVHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.ResourceBundle;
+
+/**
+ * @author Nicolò Molinari, Datatellers.
+ * Test class for ../src/main/java/helpers/CSVHandler.java
+ * Detailed information can be found inside comments and log.
+ */
+public class CSVHandlerTest {
+
+    private ResourceBundle rb = ResourceBundle.getBundle("test");
+    private static final Logger LOG = LogManager.getLogger(CSVHandler.class.getName());
+
+    @Test
+    public void parseStationsCSVTest()
+    {
+        LOG.debug("parseStationCSVTest execution started.");
+        HashMap<String, ArrayList<String>> stationMeta = new CSVHandler().parseStationsCSV();
+        String[] stationsIDs = rb.getString("odh.station.test.IDs").replace("{", "").replace("}", "").split(",");
+
+        LOG.debug("Checking CSV parsing outputs as expected.");
+        for (String stationID : stationsIDs)
+        {
+            Assert.assertTrue(stationMeta.keySet().contains(stationID));
+            Assert.assertTrue(stationMeta.get(stationID).size() <= 5);
+        }
+        LOG.debug("parseStationCSVTest execution terminated.");
+    }
+
+    @Test
+    public void parseTypesCSVTest()
+    {
+        LOG.debug("parseTypesCSVTest execution started.");
+        String unity = rb.getString("odh.csv.unity").replace("{","").replace("}","");
+        ArrayList<String> typesMeta = new CSVHandler().parseTypesCSV();
+
+        LOG.debug("Checking CSV parsing outputs as expected.");
+        for (String meta: typesMeta)
+        {
+            if (meta.split(",")[0].equals("6"))
+            {
+                Assert.assertNotEquals(unity, meta.split(",")[3]);
+            }else{
+                Assert.assertEquals(unity, meta.split(",")[3]);
+            }
+            Assert.assertTrue(meta.split(",").length <= 4);
+            LOG.debug("parseTypesCSVTest execution terminated.");
+        }
+    }
+}
---
+++ data-collectors/environment-appa/appatn_opendata/src/test/java/DataFetcherTest.java
@@ -0,0 +1,111 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import info.datatellers.appatn.helpers.DateHelper;
+import info.datatellers.appatn.opendata.DataFetcher;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+/**
+ * @author Nicolò Molinari, Datatellers.
+ * Test class for ../src/main/java/info/datatellers/appatn/DataFetcher.java
+ * Detailed information can be found in comments and log.
+ */
+public class DataFetcherTest {
+
+    private ResourceBundle rb = ResourceBundle.getBundle("test");
+    private static final Logger LOG = LogManager.getLogger(DataFetcherTest.class.getName());
+
+    @Test
+    public void fetchTest()
+    {
+        LOG.debug("DataFetcherTest class execution started.");
+        DataFetcher dataFetcher = new DataFetcher();
+        LOG.debug("Fetching json...");
+        ArrayList<JsonElement> jsonStations = dataFetcher.fetchStations(new DateHelper().getTestDate());
+        String jsonIndex = rb.getString("odh.json.test.stazione").replace("{", "").replace("}", "");
+
+        JsonObject jsonStation = (JsonObject) jsonStations.get(0);
+        LOG.debug("Checking that at this level the fetched json contains a single JsonArray.");
+        Assert.assertTrue(jsonStation.get(jsonIndex).isJsonArray());
+        Assert.assertEquals(1, ((JsonArray) jsonStation.get(jsonIndex)).size());
+
+        LOG.debug("Checking that at this level the JsonArray contains five JsonObjects.");
+        Assert.assertEquals(5, ((JsonObject )((JsonArray) jsonStation.get(jsonIndex)).get(0)).size());
+
+        LOG.debug("Checking the five JsonObjects contain the keys we need to access them.");
+        Assert.assertTrue(((JsonObject) ((JsonArray) jsonStation.get(jsonIndex)).get(0)).keySet().contains("nome"));
+        Assert.assertTrue(((JsonObject) ((JsonArray) jsonStation.get(jsonIndex)).get(0)).keySet().contains("citta"));
+        Assert.assertTrue(((JsonObject) ((JsonArray) jsonStation.get(jsonIndex)).get(0)).keySet().contains("indirizzo"));
+        Assert.assertTrue(((JsonObject) ((JsonArray) jsonStation.get(jsonIndex)).get(0)).keySet().contains("data_ultima_acquisizione"));
+        Assert.assertTrue(((JsonObject) ((JsonArray) jsonStation.get(jsonIndex)).get(0)).keySet().contains("dati"));
+
+        LOG.debug("Checking the JsonObject contains the desired retrieval date.");
+        Assert.assertTrue(((JsonObject)((JsonObject) ((JsonArray) jsonStation.get(jsonIndex)).get(0)).get("dati")).keySet().contains(new DateHelper().getTestDate()));
+
+        LOG.debug("Checking that every date branch contains 24 hours branches.");
+        LOG.debug("Checking that measurement values contained inside hours branch are not null.");
+
+        String[] stationsIDs = rb.getString("odh.station.test.IDs").replace("{", "").replace("}", "").split(",");
+        for (String stationsID : stationsIDs)
+        {
+            String[] specificStationPolluters = rb.getString("odh.station.test." + stationsID).
+                    replace("{", "").replace("}", "").split(",");
+
+            for (int hour = 1; hour <= 24; hour++)
+            {
+                if (hour < 10)
+                {
+                    //Checking that every date branch contains 24 hours branches.
+                    Assert.assertTrue(((JsonObject)((JsonObject)((JsonObject) ((JsonArray) jsonStation.get(jsonIndex)).get(0)).get("dati"))
+                            .get(new DateHelper().getTestDate())).keySet().contains("0" + String.valueOf(hour)));
+                    for (String specificStationPolluter : specificStationPolluters)
+                    {
+                        if ((((JsonObject) ((JsonObject) ((JsonObject) ((JsonObject) ((JsonArray) jsonStation.get(jsonIndex)).get(0)).get("dati"))
+                                .get(new DateHelper().getTestDate())).get(String.valueOf("0" + hour))).get(specificStationPolluter)) == null)
+                        {
+                            /*  Some measurements are not collected in some hours, for some unknown reason.
+                             I guess there's a minimum tolerance to be satisfied in order for the measurement
+                             to be collected.
+                             */
+                            LOG.debug("Null value. Value was not collected.");
+                        }else
+                        {
+                            //Checking that measurement values contained inside hours branch are not null.
+                            Assert.assertNotNull(((JsonObject) ((JsonObject) ((JsonObject) ((JsonObject) ((JsonArray) jsonStation.get(jsonIndex)).get(0)).get("dati"))
+                                    .get(new DateHelper().getTestDate())).get(String.valueOf("0" + hour))).get(specificStationPolluter));
+                        }
+                    }
+                }else{
+                    Assert.assertTrue(((JsonObject)((JsonObject)((JsonObject) ((JsonArray) jsonStation.get(jsonIndex)).get(0)).get("dati"))
+                            .get(new DateHelper().getTestDate())).keySet().contains(String.valueOf(hour)));
+                    for (String specificStationPolluter : specificStationPolluters)
+                    {
+                        if ((((JsonObject) ((JsonObject) ((JsonObject) ((JsonObject) ((JsonArray) jsonStation.get(jsonIndex)).get(0)).get("dati"))
+                                .get(new DateHelper().getTestDate())).get(String.valueOf(hour))).get(specificStationPolluter)) == null)
+                        {
+                            /*
+                            Some measurements are not collected in some hours, for some unknown reason.
+                            I guess there's a minimum tolerance to be satisfied in order for the measurement
+                            to be collected. Same in DataPusherTest, see lines 139-150.
+                             */
+                            LOG.debug("Null value. Polluter: " + "\"" + specificStationPolluter + "\"" + " field in json is missing: value was not collected on date: "
+                                    + new DateHelper().getTestDate() + " at station: " + ((((JsonObject) ((JsonArray) jsonStation.get(jsonIndex)).get(0)).get("nome")) + "."));
+                        }else
+                        {
+                            //Checking that measurement values contained inside hours branch are not null.
+                            Assert.assertNotNull(((JsonObject) ((JsonObject) ((JsonObject) ((JsonObject) ((JsonArray) jsonStation.get(jsonIndex)).get(0)).get("dati"))
+                                    .get(new DateHelper().getTestDate())).get(String.valueOf(hour))).get(specificStationPolluter));
+                        }
+                    }
+                }
+            }
+        }
+        LOG.debug("Test execution terminated.");
+    }
+}
