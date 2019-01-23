366d89dc9a3cf04871342abbd253cc7fd98fcd2e
Alex Lanz <alex@krumer.it>
2019-01-23 14:13:14 +0100

    Avoid log file error because of not existing folder

--- docker/entrypoint-java.sh
+++ docker/entrypoint-java.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-export MAVEN_CONFIG="$HOME"
-
 mkdir -p ~/.m2
-
 cat > ~/.m2/settings.xml << EOF
 <settings>
     <localRepository>$PWD/docker/.m2</localRepository>
 </settings>
 EOF
+
+export MAVEN_CONFIG="$HOME"
 
 /bin/bash -c "/usr/local/bin/mvn-entrypoint.sh $@"
