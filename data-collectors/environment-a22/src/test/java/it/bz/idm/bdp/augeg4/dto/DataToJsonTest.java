ce07d5c318e00dfdf9ec0dac49fc194ed56cea0b
npedot <nicola.pedot@gmail.com>
2019-05-02 11:40:25 +0200

    fixes to Europe test TimeZone

--- data-collectors/environment-a22/src/test/java/it/bz/idm/bdp/augeg4/dto/DataToJsonTest.java
+++ data-collectors/environment-a22/src/test/java/it/bz/idm/bdp/augeg4/dto/DataToJsonTest.java
@@ -1,17 +1,10 @@
 package it.bz.idm.bdp.augeg4.dto;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import it.bz.idm.bdp.augeg4.dto.fromauge.AugeG4FromAlgorabDataDto;
-
-import it.bz.idm.bdp.augeg4.dto.fromauge.RawResVal;
 import it.bz.idm.bdp.augeg4.dto.toauge.AugeG4LinearizedDataDto;
 import it.bz.idm.bdp.augeg4.dto.toauge.LinearResVal;
-import org.junit.Ignore;
 import org.junit.Test;
-import static junit.framework.TestCase.assertEquals;
 
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -21,7 +14,9 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
+import java.util.TimeZone;
+
+import static junit.framework.TestCase.assertEquals;
 
 public class DataToJsonTest {
 
@@ -32,22 +27,22 @@ public class DataToJsonTest {
         acq.setTime(1555320011885l);
 
         AugeG4LinearizedDataDto augeG4LinearizedDataDto = new AugeG4LinearizedDataDto(
-                "AIRQ01", acq,acq, Arrays.asList(
-                new LinearResVal(101,4.1),
-                new LinearResVal(102,3.7)
-                        )
-        );
+                "AIRQ01", acq, acq, Arrays.asList(
+                new LinearResVal(101, 1.3, 4.1),
+                new LinearResVal(102, 2.1, 3.7)
+        ));
 
         URL resource = DataFromJsonTest.class.getResource("/dataToAlgorab.json");
         String content = new String(Files.readAllBytes(Paths.get(resource.toURI())));
 
         ObjectMapper mapper = new ObjectMapper();
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+        dateFormat.setTimeZone(TimeZone.getTimeZone("Europe/Rome"));
         mapper.setDateFormat(dateFormat);
         // when
         String json = mapper.writeValueAsString(Arrays.asList(augeG4LinearizedDataDto));
 
         // then
-        assertEquals(content,json);
+        assertEquals(content, json);
     }
 }
