9f0d6b3ac5269daf5a0f4f2d5bb85e8c9c9abef4
bertolla <p.bertolla@noi.bz.it>
2019-05-31 11:14:02 +0200

    moved and renamed integrationtests to follow maven conventions

---
+++ data-collectors/environment-a22/src/test/java/it/bz/idm/bdp/augeg4/JobSchedulerWithMockedRetrieverIT.java
@@ -0,0 +1,39 @@
+package it.bz.idm.bdp.augeg4;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+
+import it.bz.idm.bdp.augeg4.face.DataConverterFace;
+import it.bz.idm.bdp.augeg4.face.DataLinearizerFace;
+import it.bz.idm.bdp.augeg4.fun.push.DataPusher;
+import it.bz.idm.bdp.augeg4.fun.retrieve.DataRetrieverMock;
+
+@ContextConfiguration(locations = {"classpath:/META-INF/spring/applicationContext.xml"})
+public class JobSchedulerWithMockedRetrieverIT extends AbstractJUnit4SpringContextTests {
+
+    @Autowired
+    private DataPusher pusher;
+
+    private DataRetrieverMock retriever = new DataRetrieverMock();
+
+    @Autowired
+    private DataLinearizerFace linearizer;
+
+    @Autowired
+    private DataConverterFace converter;
+
+    @Test
+    public void test_scheduled_push_data_with_mocked_retriever () throws Exception {
+        // given
+        JobScheduler js = new JobScheduler(pusher, retriever, linearizer, converter);
+        js.mockDataRetrievedFromAlgorab();
+        js.pushStations();
+
+        // when
+        js.pushData();
+
+        // then no exception is thrown
+    }
+}
