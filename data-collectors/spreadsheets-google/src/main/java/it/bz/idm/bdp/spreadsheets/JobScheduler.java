61d6a7963ff975bf950274f095ffa09e183c0760
bertolla <p.bertolla@noi.bz.it>
2019-07-17 16:59:18 +0200

    initial import of spreadhseet data collector

---
+++ data-collectors/spreadsheets-google/src/main/java/it/bz/idm/bdp/spreadsheets/JobScheduler.java
@@ -0,0 +1,96 @@
+package it.bz.idm.bdp.spreadsheets;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import it.bz.idm.bdp.dto.StationDto;
+import it.bz.idm.bdp.dto.StationList;
+
+@Service
+public class JobScheduler {
+
+	@Autowired
+	private GoogleSpreadSheetDataFetcher googleClient;
+
+	@Value("${spreadsheetId}")
+	private String origin;
+
+	@Autowired
+	private ODHClient odhClient;
+
+	@Value("${headers.addressId}")
+	private String addressId;
+
+	/**
+	 * scheduled job which syncs odh with the spreadsheet
+	 */
+	public void syncData() {
+
+		List<List<Object>> values = googleClient.getWholeSheet().getValues();
+		List<StationDto> odhStations = odhClient.fetchStations(odhClient.getIntegreenTypology(), origin);
+		StationList dtos = mapData(values, odhStations);
+		odhClient.syncStations(dtos);
+
+	}
+
+	/**
+	 * @param spreadSheetValues values to map to a {@link StationDto}
+	 * @param existingOdhStations {@link StationDto}'s of this spreadsheet which already exist in odh
+	 * @return list of all valid rows in form of {@link StationDto}
+	 */
+	private StationList mapData(List<List<Object>> spreadSheetValues, List<StationDto> existingOdhStations) {
+		StationList dtos = new StationList();
+		Map<String, Short> headerMapping = null;
+		Set<Integer> missingPositions = new HashSet<>();
+		int i = 0;
+		for (final List<Object> row : spreadSheetValues) {
+			if (i == 0) {
+				headerMapping = getHeaderMapping(row);
+				if (headerMapping.isEmpty() || !(headerMapping.containsKey("address")||(headerMapping.containsKey("longitude") && headerMapping.containsKey("latitdude")))){
+					throw new IllegalStateException("Spreadsheet does not conform to rules");
+				}
+			} else {
+				final StationDto dto = odhClient.mapStation(headerMapping, row);
+				if (!existingOdhStations.contains(dto) && dto.getLongitude() == null) {
+					try {
+						odhClient.guessPositionByAddress(dto);
+					}catch(final IllegalStateException ex) {
+						missingPositions.add(i);
+						i++;
+						continue;
+					}
+				}
+				odhClient.guessLanguages(dto.getMetaData());
+				odhClient.mergeTranslations(dto.getMetaData(),headerMapping);
+				if (existingOdhStations.contains(dto)||(dto.getLongitude() != null && dto.getLatitude() != null)) {
+					dtos.add(dto);
+				}
+			}
+			i++;
+		}
+		googleClient.markMissing(missingPositions,headerMapping.get(addressId).intValue());
+		return dtos;
+	}
+
+	/**
+	 * @param row values of column header row
+	 * @return mapping of column name with column position in sheet
+	 */
+	private Map<String, Short> getHeaderMapping(List<Object> row) {
+		Map<String, Short> mapping = new HashMap<>();
+		short count = 0;
+		for (Object header : row) {
+			mapping.put(header.toString().toLowerCase(), count);
+			count++;
+		}
+		return mapping;
+	}
+
+}
