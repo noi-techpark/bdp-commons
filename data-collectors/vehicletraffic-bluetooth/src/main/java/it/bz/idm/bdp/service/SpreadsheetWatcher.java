fff2835f24f53fa69d4b1f453262935c899c0d57
bertolla <p.bertolla@noi.bz.it>
2019-10-31 15:35:15 +0100

    scheduled job for cahnnel renewal

--- data-collectors/vehicletraffic-bluetooth/src/main/java/it/bz/idm/bdp/service/SpreadsheetWatcher.java
+++ data-collectors/vehicletraffic-bluetooth/src/main/java/it/bz/idm/bdp/service/SpreadsheetWatcher.java
@@ -1,10 +1,11 @@
 package it.bz.idm.bdp.service;
 
 import java.io.IOException;
-import java.security.GeneralSecurityException;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.google.api.client.auth.oauth2.Credential;
@@ -17,20 +18,34 @@ import it.bz.idm.bdp.util.GoogleAuthenticator;
 
 @Lazy
 @Service
-public class SpreadsheetWatcher extends GoogleAuthenticator{
+public class SpreadsheetWatcher extends GoogleAuthenticator {
 
-    private Drive service;
+	private Drive service;
 
-    @Value("${spreadsheetId}")
-    private String spreadsheetId;
+	@Value("${spreadsheetId}")
+	private String spreadsheetId;
 
-    public void registerWatch(Channel c) throws IOException, GeneralSecurityException {
-        service.files().watch(spreadsheetId, c).execute();
-    }
+	@Scheduled(fixedDelay = 1000 * 60 * 55, initialDelay = 1000)
+	public void watchBluetoothBoxesSpreadsheet() {
+		Channel channel = new Channel();
+		channel.setId(UUID.randomUUID().toString());
+		channel.setType("web_hook");
+		channel.setAddress("https://boxes.opendatahub.bz.it/dc-vehicletraffic-bluetooth/trigger");
+		registerWatch(channel);
+	}
+
+	public void registerWatch(Channel c) {
+		try {
+			service.files().watch(spreadsheetId, c).execute();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	@Override
 	public void initGoogleClient(NetHttpTransport HTTP_TRANSPORT, JsonFactory JSON_FACTORY, Credential credential)
 			throws IOException {
-		service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).setApplicationName("google spreadsheet collector").build();
+		service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+				.setApplicationName("google spreadsheet collector").build();
 	}
 }
