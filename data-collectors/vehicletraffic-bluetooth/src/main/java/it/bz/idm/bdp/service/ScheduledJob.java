6c0b0b961607294cb747b7ecff32a0ea33bdb79c
bertolla <p.bertolla@noi.bz.it>
2019-11-04 16:12:12 +0100

    [vehicletraffic-bluetooth] move scheduling to xml

---
+++ data-collectors/vehicletraffic-bluetooth/src/main/java/it/bz/idm/bdp/service/ScheduledJob.java
@@ -0,0 +1,27 @@
+package it.bz.idm.bdp.service;
+
+import java.util.UUID;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.google.api.services.drive.model.Channel;
+
+@Service
+public class ScheduledJob {
+	private Logger logger = Logger.getLogger(ScheduledJob.class);
+
+	@Autowired
+	private SpreadsheetWatcher watcher;
+
+	public void watchBluetoothBoxesSpreadsheet() {
+		logger.debug("Run scheduled channel creation");
+		Channel channel = new Channel();
+		channel.setId(UUID.randomUUID().toString());
+		channel.setType("web_hook");
+		channel.setAddress("https://boxes.opendatahub.bz.it/dc-vehicletraffic-bluetooth/trigger");
+		watcher.registerWatch(channel);
+		logger.debug("Channel created and registered");
+	}
+}
--- data-collectors/vehicletraffic-bluetooth/src/main/java/it/bz/idm/bdp/service/SpreadsheetWatcher.java
+++ data-collectors/vehicletraffic-bluetooth/src/main/java/it/bz/idm/bdp/service/SpreadsheetWatcher.java
@@ -1,11 +1,9 @@
 package it.bz.idm.bdp.service;
 
 import java.io.IOException;
-import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.google.api.client.auth.oauth2.Credential;
@@ -24,15 +22,6 @@ public class SpreadsheetWatcher extends GoogleAuthenticator {
 
 	@Value("${spreadsheetId}")
 	private String spreadsheetId;
-
-	@Scheduled(fixedDelay = 1000 * 60 * 55, initialDelay = 1000)
-	public void watchBluetoothBoxesSpreadsheet() {
-		Channel channel = new Channel();
-		channel.setId(UUID.randomUUID().toString());
-		channel.setType("web_hook");
-		channel.setAddress("https://boxes.opendatahub.bz.it/dc-vehicletraffic-bluetooth/trigger");
-		registerWatch(channel);
-	}
 
 	public void registerWatch(Channel c) {
 		try {
