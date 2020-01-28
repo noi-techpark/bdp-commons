1110728b3f9d938f0125d673c42d4397f5cf6066
bertolla <p.bertolla@noi.bz.it>
2020-01-28 08:58:28 +0100

    remove unused lib

--- data-collectors/spreadsheets-google/src/main/java/it/bz/odh/spreadsheets/TriggerController.java
+++ data-collectors/spreadsheets-google/src/main/java/it/bz/odh/spreadsheets/TriggerController.java
@@ -3,7 +3,6 @@ package it.bz.odh.spreadsheets;
 import java.util.Date;
 
 import org.apache.log4j.Logger;
-import org.mortbay.log.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
