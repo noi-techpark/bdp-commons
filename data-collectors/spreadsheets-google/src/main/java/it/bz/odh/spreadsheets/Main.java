b3f7a91286feab7ee95cf4b557df34e15091deec
bertolla <p.bertolla@noi.bz.it>
2020-02-24 17:35:23 +0100

    change datatype mapping

--- data-collectors/spreadsheets-google/src/main/java/it/bz/odh/spreadsheets/Main.java
+++ data-collectors/spreadsheets-google/src/main/java/it/bz/odh/spreadsheets/Main.java
@@ -1,7 +1,10 @@
 package it.bz.odh.spreadsheets;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,8 +14,12 @@ import org.springframework.stereotype.Service;
 import com.google.api.services.sheets.v4.model.Sheet;
 import com.google.api.services.sheets.v4.model.Spreadsheet;
 
+import it.bz.idm.bdp.dto.DataMapDto;
 import it.bz.idm.bdp.dto.DataTypeDto;
+import it.bz.idm.bdp.dto.RecordDtoImpl;
+import it.bz.idm.bdp.dto.SimpleRecordDto;
 import it.bz.idm.bdp.dto.StationList;
+import it.bz.odh.spreadsheets.dto.DataTypeWrapperDto;
 import it.bz.odh.spreadsheets.dto.MappingResult;
 import it.bz.odh.spreadsheets.services.GoogleSpreadSheetDataFetcher;
 import it.bz.odh.spreadsheets.services.ODHClient;
@@ -41,7 +48,7 @@ public class Main {
     public void syncData() {
         Spreadsheet fetchedSpreadSheet = (Spreadsheet) googleClient.fetchSheet();
         StationList dtos = new StationList();
-        List <DataTypeDto> types = new ArrayList<DataTypeDto>();
+        List <DataTypeWrapperDto> types = new ArrayList<DataTypeWrapperDto>();
         for (Sheet sheet : fetchedSpreadSheet.getSheets()){
             try {
                 List<List<Object>> values = googleClient.getWholeSheet(sheet.getProperties().getTitle()).getValues();
@@ -50,7 +57,7 @@ public class Main {
                 MappingResult result = mappingUtil.mapSheet(values,sheet);
                 if (!result.getStationDtos().isEmpty())
                     dtos.addAll(result.getStationDtos());
-                if (result.getDataType() != null && result.getDataType().getName() != null) {
+                if (result.getDataType() != null) {
                     types.add(result.getDataType());
                 }
             }catch(Exception ex) {
@@ -60,7 +67,19 @@ public class Main {
         }
         if (!dtos.isEmpty())
             odhClient.syncStations(dtos);
-        if (!types.isEmpty())
-            odhClient.syncDataTypes(types);
+        if (!types.isEmpty()) {
+            List<DataTypeDto> dTypes = types.stream().map(mapper).collect(Collectors.toList());
+            odhClient.syncDataTypes(dTypes);
+        }
+        DataMapDto<? extends RecordDtoImpl> dto = new DataMapDto<RecordDtoImpl>();
+        for (DataTypeWrapperDto typeDto: types) {
+            SimpleRecordDto simpleRecordDto = new SimpleRecordDto(new Date().getTime(), typeDto.getSheetName());
+            dto.addRecord(dtos.get(0).getId(), typeDto.getType().getName(), simpleRecordDto);
+        }
+        odhClient.pushData(dto);
     }
+    Function<DataTypeWrapperDto,DataTypeDto> mapper = (dto) -> {
+        return dto.getType();
+    };
+
 }
--- data-collectors/spreadsheets-google/src/main/java/it/bz/odh/spreadsheets/dto/MappingResult.java
+++ data-collectors/spreadsheets-google/src/main/java/it/bz/odh/spreadsheets/dto/MappingResult.java
@@ -3,13 +3,12 @@ package it.bz.odh.spreadsheets.dto;
 import java.util.ArrayList;
 import java.util.List;
 
-import it.bz.idm.bdp.dto.DataTypeDto;
 import it.bz.idm.bdp.dto.StationDto;
 
 public class MappingResult {
     private List<StationDto> stationDtos = new ArrayList<StationDto>();
 
-    private DataTypeDto dataType;
+    private DataTypeWrapperDto dataType;
 
     public List<StationDto> getStationDtos() {
         return stationDtos;
@@ -19,10 +18,11 @@ public class MappingResult {
         this.stationDtos = stationDtos;
     }
 
-    public void setDataType(DataTypeDto type) {
-        this.dataType=type;
-    }
-    public DataTypeDto getDataType() {
+    public DataTypeWrapperDto getDataType() {
         return dataType;
+    }
+
+    public void setDataType(DataTypeWrapperDto dataType) {
+        this.dataType = dataType;
     }
 }
