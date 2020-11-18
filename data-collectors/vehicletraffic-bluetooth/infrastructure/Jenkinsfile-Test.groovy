9a04d00be4389b0dd8babf0c8f4331829734d7e7
bertolla <p.bertolla@noi.bz.it>
2020-11-18 10:39:24 +0100

    add testing pipeline / infrastructure

---
+++ data-collectors/vehicletraffic-bluetooth/infrastructure/Jenkinsfile-Test.groovy
@@ -0,0 +1,68 @@
+pipeline {
+    agent any
+    
+    environment {
+        SERVER_PORT="1007"
+        PROJECT = "vehicletraffic-bluetooth"
+        PROJECT_FOLDER = "data-collectors/${PROJECT}"
+        ARTIFACT_NAME = "dc-${PROJECT}"
+        GOOGLE_SECRET=credentials('spreadsheets.client_secret.json')
+        GOOGLE_CREDENTIALS=credentials('google-spreadsheet-api-credentials')
+        ENCRYPTION_KEY = credentials('bluetooth-encryption-key')
+        DOCKER_IMAGE = '755952719952.dkr.ecr.eu-west-1.amazonaws.com/dc-vehicletraffic-bluetooth'
+        DOCKER_TAG = "test-$BUILD_NUMBER"
+    }
+
+    stages {
+        stage('Configure') {
+            steps {
+                sh """
+                    cd ${PROJECT_FOLDER}
+                    echo 'SERVER_PORT=${SERVER_PORT}' > .env
+                    echo 'DOCKER_IMAGE=${DOCKER_IMAGE}' >> .env
+                    echo 'DOCKER_TAG=${DOCKER_TAG}' >> .env
+                    echo 'LOG_LEVEL=DEBUG' >> .env
+                    echo 'ARTIFACT_NAME=${ARTIFACT_NAME}' >> .env
+                    echo 'spreadsheetId=10qBMw28HFWJZen6_CBKT1ln5s8XdygcoC3buXas_8SA' >> .env
+                    echo 'spreadsheet_range=A1:Z' >> .env
+                    echo 'spreadsheet_requiredFields=id,longitude,latitude' >> .env
+                    echo 'spreadsheet_sheetName=boxes' >> .env
+                    echo 'encryption_key=${ENCRYPTION_KEY}' >> .env
+                    echo 'spreadsheet_notificationUrl=https://spreadsheets.testingmachine.eu/centro-trevi/trigger' >> .env
+                    echo 'stationtype=BluetoothStation' >> .env
+                    echo 'datatype=vehicle detection' >> .env
+                    echo 'origin=bluetoothbox' >> .env
+                    echo -n 'provenance_version=' >> .env
+                    xmlstarlet sel -N pom=http://maven.apache.org/POM/4.0.0 -t -v '/pom:project/pom:version' pom.xml >> .env
+                    echo '' >> .env
+                    echo -n 'provenance_name=' >> .env 
+                    xmlstarlet sel -N pom=http://maven.apache.org/POM/4.0.0 -t -v '/pom:project/pom:artifactId' pom.xml >> .env
+                    echo '' >> .env
+                """
+                
+                sh "cat ${GOOGLE_SECRET} > ${PROJECT_FOLDER}/src/main/resources/META-INF/spring/client_secret.json"
+                sh """cat "${GOOGLE_CREDENTIALS}" > "${PROJECT_FOLDER}"/src/main/resources/META-INF/credentials/StoredCredential"""
+            }
+        }
+        stage('Test & Build') {
+            steps {
+                sh """
+                    cd ${PROJECT_FOLDER}
+                    aws ecr get-login --region eu-west-1 --no-include-email | bash
+                    docker-compose --no-ansi -f infrastructure/docker-compose.build.yml build --pull
+                    docker-compose --no-ansi -f infrastructure/docker-compose.build.yml push
+                """
+            }
+        }
+        stage('Deploy') {
+            steps {
+               sshagent(['jenkins-ssh-key']) {
+                    sh """
+                        (cd ${PROJECT_FOLDER}/infrastructure/ansible && ansible-galaxy install -f -r requirements.yml)
+                        (cd ${PROJECT_FOLDER}/infrastructure/ansible && ansible-playbook --limit=test deploy.yml --extra-vars "release_name=${BUILD_NUMBER} project_name=${PROJECT}")
+                    """
+                }
+            }
+        }
+    }
+}
