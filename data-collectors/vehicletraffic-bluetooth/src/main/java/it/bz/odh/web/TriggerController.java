1d85f04910fe44ec27239bdce2840f90c2966c49
bertolla <p.bertolla@noi.bz.it>
2020-11-23 13:53:59 +0100

    add debug logging

--- data-collectors/vehicletraffic-bluetooth/src/main/java/it/bz/odh/web/TriggerController.java
+++ data-collectors/vehicletraffic-bluetooth/src/main/java/it/bz/odh/web/TriggerController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -23,6 +25,8 @@ import it.bz.odh.util.BluetoothMappingUtil;
 @Controller
 @EnableWebMvc
 public class TriggerController {
+	
+	private Logger logger = LogManager.getLogger(TriggerController.class);
 
 	@Autowired
 	private OddsPusher pusher;
@@ -45,17 +49,24 @@ public class TriggerController {
 	@RequestMapping(method = RequestMethod.POST)
 	public @ResponseBody void post(@RequestBody(required = false) GooglePushDto gDto,
 			@RequestHeader(required = true, value = "x-goog-changed") String whatChanged) {
+		logger.debug("Sync triggered");
 		if ("content".equals(whatChanged)) {
+			logger.debug("Call is content related");
 			metaUtil.setCachedData(null);
+			logger.debug("Fetch Stations from odh");
 			List<StationDto> odhStations = pusher.fetchStations(pusher.getIntegreenTypology(), origin);
 			List<String> stationIds = odhStations.stream().map(StationDto::getId).collect(Collectors.toList());
+			logger.debug("Found "+stationIds.size()+" Bluetoothboxes");
 			StationList stations = new StationList();
 			for (Map<String, String> entry : metaUtil.getValidEntries()) {
 				String stationId = entry.get("id");
+				logger.debug("Extract station with id: "+stationId);
 				if (!stationIds.contains(stationId))
 					continue;
+				logger.debug("Map station with id "+stationId);
 				Double[] coordinatesByIdentifier = metaUtil.getCoordinatesByIdentifier(stationId);
 				Map<String, Object> metaDataByIdentifier = metaUtil.getMetaDataByIdentifier(stationId);
+				logger.debug("Start merging translations");
 				metaUtil.mergeTranslations(metaDataByIdentifier);
 				StationDto dto = new StationDto();
 				dto.setName(stationId);
@@ -65,9 +76,13 @@ public class TriggerController {
 				if (!metaDataByIdentifier.isEmpty())
 					dto.setMetaData(metaDataByIdentifier);
 				stations.add(dto);
+				logger.debug("Dto created and added");
 			}
-			if (!stations.isEmpty())
+			if (!stations.isEmpty()) {
+				logger.debug("Push data to odh");
 				pusher.syncStations(stations);
+				logger.debug("Finished pushing to odh");
+			}
 		}
 	}
 
