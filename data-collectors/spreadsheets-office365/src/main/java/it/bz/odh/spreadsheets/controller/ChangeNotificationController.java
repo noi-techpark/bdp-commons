a2ebe1f4229e30432d8511befa07e64d6a0304b1
dulvui <simon.dalvai@gmail.com.com>
2021-01-06 09:46:51 +0100

    Disables Notifiactions controller for now, because manual syncing is used and web-mvc is disabled

--- data-collectors/spreadsheets-office365/src/main/java/it/bz/odh/spreadsheets/controller/ChangeNotificationController.java
+++ data-collectors/spreadsheets-office365/src/main/java/it/bz/odh/spreadsheets/controller/ChangeNotificationController.java
@@ -6,22 +6,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-@RestController
-@RequestMapping("/trigger")
+//@RestController
+//@RequestMapping("/trigger")
 public class ChangeNotificationController {
 
 //    private Logger logger = Logger.getLogger(ChangeNotificationController.class);
 
 
-    @PostMapping(value = "/notification",headers = { "content-type=text/plain" })
-    @ResponseBody
+//    @PostMapping(value = "/notification",headers = { "content-type=text/plain" })
+//    @ResponseBody
     public ResponseEntity<String> handleValidation(@RequestParam("validationToken") String validationToken) {
         System.out.println(validationToken);
         return ResponseEntity.ok().contentType(MediaType.TEXT_PLAIN)
                 .body(StringEscapeUtils.escapeHtml4(validationToken));
     }
-    @PostMapping("/notification")
-    @ResponseBody
+
+
+
+
+//    @PostMapping("/notification")
+//    @ResponseBody
     public ResponseEntity<String> handleNotification(@RequestBody() final String jsonString){
         System.out.println(jsonString);
         return ResponseEntity.ok().body("");
