f9d52021d1d660ca4a1c1e67da520804b04bdfe4
bertolla <p.bertolla@noi.bz.it>
2021-01-28 13:40:33 +0100

    remove unneeded port config

--- data-collectors/environment-appa/appatn_opendata/infrastructure/Jenkinsfile-Test.groovy
+++ data-collectors/environment-appa/appatn_opendata/infrastructure/Jenkinsfile-Test.groovy
@@ -2,7 +2,6 @@ pipeline {
     agent any
     
     environment {
-        SERVER_PORT="1013"
         PROJECT = "appatn_opendata"
         PROJECT_FOLDER = "data-collectors/environment-appa/${PROJECT}"
         ARTIFACT_NAME = "dc-${PROJECT}"
@@ -17,7 +16,6 @@ pipeline {
                 sh """
                     cd ${PROJECT_FOLDER}
                     echo 'COMPOSE_PROJECT_NAME=${PROJECT}' > .env
-                    echo 'SERVER_PORT=${SERVER_PORT}' >> .env
                     echo 'DOCKER_IMAGE=${DOCKER_IMAGE}' >> .env
                     echo 'DOCKER_TAG=${DOCKER_TAG}' >> .env
                     echo 'LOG_LEVEL=debug' >> .env
