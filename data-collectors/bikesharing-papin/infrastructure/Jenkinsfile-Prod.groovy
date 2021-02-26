dc415e6ebd36754225f1acb0587de9a03d11b9ce
bertolla <p.bertolla@noi.bz.it>
2021-02-26 16:15:03 +0100

    add prod pipeline

--- data-collectors/bikesharing-papin/infrastructure/Jenkinsfile-Prod.groovy
+++ data-collectors/bikesharing-papin/infrastructure/Jenkinsfile-Prod.groovy
@@ -6,8 +6,8 @@ pipeline {
         PROJECT_FOLDER = "data-collectors/${PROJECT}"
         ARTIFACT_NAME = "dc-${PROJECT}"
         DOCKER_IMAGE = '755952719952.dkr.ecr.eu-west-1.amazonaws.com/dc-bikesharing-papin'
-        DOCKER_TAG = "test-$BUILD_NUMBER"
-        DATACOLLECTORS_CLIENT_SECRET = credentials('keycloak-datacollectors-secret')
+        DOCKER_TAG = "prod-$BUILD_NUMBER"
+        DATACOLLECTORS_CLIENT_SECRET = credentials('keycloak-datacollectors-secret-prod')
         AUTH_TOKEN = credentials('bikesharing-papin')
     }
 
@@ -20,12 +20,12 @@ pipeline {
                     echo 'SERVER_PORT=${SERVER_PORT}' >> .env
                     echo 'DOCKER_IMAGE=${DOCKER_IMAGE}' >> .env
                     echo 'DOCKER_TAG=${DOCKER_TAG}' >> .env
-                    echo 'LOG_LEVEL=debug' >> .env
+                    echo 'LOG_LEVEL=info' >> .env
                     echo 'ARTIFACT_NAME=${ARTIFACT_NAME}' >> .env
                     echo 'app_auth_token=${AUTH_TOKEN}' >> .env
                     echo 'app_origin=BIKE_SHARING_PAPIN' >> .env
-                    echo 'authorizationUri=https://auth.opendatahub.testingmachine.eu/auth' >> .env
-                    echo 'tokenUri=https://auth.opendatahub.testingmachine.eu/auth/realms/noi/protocol/openid-connect/token' >> .env 
+                    echo 'authorizationUri=https://auth.opendatahub.bz.it/auth' >> .env
+                    echo 'tokenUri=https://auth.opendatahub.bz.it/auth/realms/noi/protocol/openid-connect/token' >> .env 
                     echo 'clientId=odh-mobility-datacollector' >> .env
                     echo 'clientName=odh-mobility-datacollector' >> .env
                     echo 'clientSecret=${DATACOLLECTORS_CLIENT_SECRET}' >> .env
@@ -36,7 +36,7 @@ pipeline {
                     echo -n 'provenance_name=' >> .env 
                     xmlstarlet sel -N pom=http://maven.apache.org/POM/4.0.0 -t -v '/pom:project/pom:artifactId' pom.xml >> .env
                     echo '' >> .env
-                    echo 'BASE_URI=https://share.opendatahub.testingmachine.eu/json' >> .env
+                    echo 'BASE_URI=https://mobility.share.opendatahub.bz.it/json' >> .env
                 """
             }
         }
@@ -55,7 +55,7 @@ pipeline {
                sshagent(['jenkins-ssh-key']) {
                     sh """
                         (cd ${PROJECT_FOLDER}/infrastructure/ansible && ansible-galaxy install -f -r requirements.yml)
-                        (cd ${PROJECT_FOLDER}/infrastructure/ansible && ansible-playbook --limit=test deploy.yml --extra-vars "release_name=${BUILD_NUMBER} project_name=${PROJECT}")
+                        (cd ${PROJECT_FOLDER}/infrastructure/ansible && ansible-playbook --limit=prod deploy.yml --extra-vars "release_name=${BUILD_NUMBER} project_name=${PROJECT}")
                     """
                 }
             }
