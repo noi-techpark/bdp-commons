105bd932c0dac4ab1a4fbae5f18defcca26ef693
bertolla <p.bertolla@noi.bz.it>
2021-04-20 09:48:51 +0200

    [#236] should fix traffic issues

--- data-collectors/traffic-a22/a22elaborations/src/main/resources/it/bz/noi/a22elaborations/read-all-stations.sql
+++ data-collectors/traffic-a22/a22elaborations/src/main/resources/it/bz/noi/a22elaborations/read-all-stations.sql
@@ -1,3 +1,1 @@
-select * 
-  from a22.a22_station
-  where exists (select timestamp from a22.a22_traffic where stationcode = code and timestamp > extract(epoch from (current_timestamp - '1 year'::interval)));
+select * from a22.get_non_dead_stations();
