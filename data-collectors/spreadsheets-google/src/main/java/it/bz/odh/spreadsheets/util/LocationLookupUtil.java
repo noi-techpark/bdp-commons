be8973bc4c6e808a40b15f155cf892634ba138fc
bertolla <p.bertolla@noi.bz.it>
2021-06-15 12:13:27 +0200

    handle nominatim exception

--- data-collectors/spreadsheets-google/src/main/java/it/bz/odh/spreadsheets/util/LocationLookupUtil.java
+++ data-collectors/spreadsheets-google/src/main/java/it/bz/odh/spreadsheets/util/LocationLookupUtil.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 
 import it.bz.idm.bdp.dto.StationDto;
 import it.bz.idm.bdp.util.LocationLookup;
+import it.bz.idm.bdp.util.NominatimException;
 import it.bz.idm.bdp.util.NominatimLocationLookupUtil;
 
 @Component
@@ -21,11 +22,16 @@ public class LocationLookupUtil {
     public void guessPositionByAddress(StationDto dto) {
         Object addressObject = dto.getMetaData().get(addressId);
         if (addressObject != null && !addressObject.toString().isEmpty()) {
-            Double[] coordinates = lookUpUtil.lookupCoordinates(addressObject.toString());
-            if (coordinates[0] != null && coordinates[1] != null) {
-                dto.setLongitude(coordinates[0]);
-                dto.setLatitude(coordinates[1]);
-            }
+            Double[] coordinates;
+			try {
+				coordinates = lookUpUtil.lookupCoordinates(addressObject.toString());
+				if (coordinates[0] != null && coordinates[1] != null) {
+	                dto.setLongitude(coordinates[0]);
+	                dto.setLatitude(coordinates[1]);
+	            }
+			} catch (NominatimException e) {
+				e.printStackTrace();
+			}
         }
     }
 
