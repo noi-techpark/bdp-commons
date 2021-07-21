84e0c6592b87888eb34b20b19897d7090074c571
bertolla <p.bertolla@noi.bz.it>
2021-07-21 09:11:00 +0200

    change logging to info

--- data-collectors/parking-a22/infrastructure/Jenkinsfile-Prod.groovy
+++ data-collectors/parking-a22/infrastructure/Jenkinsfile-Prod.groovy
@@ -19,7 +19,7 @@ pipeline {
                     echo 'COMPOSE_PROJECT_NAME=${PROJECT}' > .env
                     echo 'DOCKER_IMAGE=${DOCKER_IMAGE}' >> .env
                     echo 'DOCKER_TAG=${DOCKER_TAG}' >> .env
-                    echo 'LOG_LEVEL=debug' >> .env
+                    echo 'LOG_LEVEL=info' >> .env
                     echo 'ARTIFACT_NAME=${ARTIFACT_NAME}' >> .env
                     echo 'authorizationUri=https://auth.opendatahub.bz.it/auth' >> .env
                     echo 'tokenUri=https://auth.opendatahub.bz.it/auth/realms/noi/protocol/openid-connect/token' >> .env 
