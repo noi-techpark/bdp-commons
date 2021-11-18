8008bd1fac503aec5aee327f243b5f8f36c22696
Peter Moser <p.moser@noi.bz.it>
2021-11-18 17:37:19 +0100

    ci: use separate file

--- Jenkinsfile-CI.groovy
+++ Jenkinsfile-CI.groovy
@@ -11,32 +11,12 @@ pipeline {
             stages {
                 stage('API v1') {
                     steps {
-                        sh 'cd webservices/bdp-api/ && mvn -B -U clean compile test'
+                        sh 'true # cd webservices/bdp-api/ && mvn -B -U clean compile test'
                     }
                 }
                 stage('Data Collectors') {
                     steps {
-                        sh '''
-                            echo
-                            echo "!!! WE TEST ALL DATA COLLECTORS THAT HAVE BEEN CHANGED SINCE LAST COMMIT !!!"
-                            echo
-                            DELTA_FILES=$(git diff --name-only HEAD "$GIT_PREVIOUS_SUCCESSFUL_COMMIT" | grep data-collectors/ || true)
-                            echo "Changed Data Collectors:"
-                            typeset -a DC_SET
-                            for RECORD in ${DELTA_FILES[@]}; do
-                                RECORD=$(echo "$RECORD" | sed -E 's#data-collectors/(.*)/.*$#\1#g')
-                                DC_SET+=("$RECORD")
-                            done
-                            DC_SET=($(printf "%s\n" "${DC_SET[@]}" | sort -u))
-                            cd data-collectors
-                            for DC_NAME in "${DC_SET[@]}"; do
-                                (cd "$DC_NAME" && mvn -B -U clean compile test)
-                            done
-                            echo
-                            echo "!!! Ready !!!"
-                            echo
-                        '''
-
+                        sh './ci-check-data-collectors.sh'
                     }
                 }
             }
