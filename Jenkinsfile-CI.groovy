52e17ff9a1a210f315ad9db74a3ba22b9335f112
Peter Moser <p.moser@noi.bz.it>
2021-11-18 17:10:31 +0100

    ci: remove duplicates from data collector array

--- Jenkinsfile-CI.groovy
+++ Jenkinsfile-CI.groovy
@@ -20,14 +20,16 @@ pipeline {
                             echo
                             echo "!!! WE TEST ALL DATA COLLECTORS THAT HAVE BEEN CHANGED SINCE LAST COMMIT !!!"
                             echo
-                            ROOT_COMMIT=$(git cherry development|head -n1|awk '{print $2}')
-                            readarray -d / -t GIT_DELTA <<< "$(git diff --name-only HEAD $ROOT_COMMIT | grep data-collectors/)"
+                            DELTA_FILES=$(git diff --name-only HEAD "$GIT_PREVIOUS_SUCCESSFUL_COMMIT" | grep data-collectors/ || true)
                             echo "Changed Data Collectors:"
-                            for DC_NAME in "${GIT_DELTA[@]}"; do
-                                echo "- $DC_NAME"
+                            declare -a DC_SET
+                            for RECORD in ${DELTA_FILES[@]}; do
+                                RECORD=$(echo "$RECORD" | sed -E 's#data-collectors/(.*)/.*$#\1#g')
+                                DC_SET+=("$RECORD")
                             done
+                            DC_SET=($(printf "%s\n" "${DC_SET[@]}" | sort -u))
                             cd data-collectors
-                            for DC_NAME in "${GIT_DELTA[@]}"; do
+                            for DC_NAME in "${DC_SET[@]}"; do
                                 (cd "$DC_NAME" && mvn -B -U clean compile test)
                             done
                             echo
