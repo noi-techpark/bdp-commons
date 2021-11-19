06f71f57bf35660aa0e66f1321c8dca3a4ee29df
Peter Moser <p.moser@noi.bz.it>
2021-11-19 11:39:46 +0100

    ci: check all data collectors on development and master

--- ci-check-data-collectors.sh
+++ ci-check-data-collectors.sh
@@ -1,32 +1,47 @@
 #!/bin/bash
 
-echo
-echo "!!! WE TEST ALL DATA COLLECTORS THAT HAVE BEEN CHANGED SINCE LAST COMMIT !!!"
-echo
-if [[ "${BRANCH_NAME}" == "development" ]]; then
-    REF_BRANCH="origin/master"
-    git fetch -- "$GIT_URL" +refs/heads/master:refs/remotes/origin/master
-elif [[ "${BRANCH_NAME}" == "master" ]]; then
-    REF_BRANCH="$GIT_PREVIOUS_SUCCESSFUL_COMMIT"
+set -e
+
+# Perform a full check on all data collectors for our main branches, development and master
+# All other branches (incl. PRs) we just go back to find the point in history where we branched
+# out, and see which folders have changed files, and then execute the test only on those 
+# data collectors.
+if [[ "${BRANCH_NAME}" == "development" || "${BRANCH_NAME}" == "master" ]]; then
+    echo
+    echo "!!! WE TEST ALL DATA COLLECTORS !!!"
+    echo
+    DC_SET=($(find data-collectors/ -mindepth 1 -maxdepth 1 -type d -not -name '.*' | sed -E 's#data-collectors/(.*)$#\1#g' | sort))
 else
-    REF_BRANCH="origin/development"
+    echo
+    echo "!!! WE TEST ALL DATA COLLECTORS THAT HAVE BEEN CHANGED SINCE LAST COMMIT !!!"
+    echo
     git fetch -- "$GIT_URL" +refs/heads/development:refs/remotes/origin/development
+    echo "Using REF_BRANCH = origin/development"
+    ROOT_COMMIT=$(git cherry origin/development|head -n1|awk '{print $2}')
+    DELTA_FILES=$(git diff --name-only HEAD "$ROOT_COMMIT" | grep data-collectors/ || true)
+    echo "Changed Data Collectors:"
+    for RECORD in ${DELTA_FILES[@]}; do
+        echo "--> Checking delta for $RECORD"
+        RECORD=$(echo "$RECORD" | sed -E 's#data-collectors/([^/]*)/.*$#\1#g')
+        DC_SET+=("$RECORD")
+    done
+    DC_SET=($(printf "%s\n" "${DC_SET[@]}" | sort -u))
 fi
 
-echo "Using REF_BRANCH = $REF_BRANCH"
 
-ROOT_COMMIT=$(git cherry $REF_BRANCH|head -n1|awk '{print $2}')
-DELTA_FILES=$(git diff --name-only HEAD "$ROOT_COMMIT" | grep data-collectors/ || true)
-echo "Changed Data Collectors:"
-for RECORD in ${DELTA_FILES[@]}; do
-    echo "--> Checking delta for $RECORD"
-    RECORD=$(echo "$RECORD" | sed -E 's#data-collectors/([^/]*)/.*$#\1#g')
-    DC_SET+=("$RECORD")
-done
-DC_SET=($(printf "%s\n" "${DC_SET[@]}" | sort -u))
 for DC_NAME in "${DC_SET[@]}"; do
-    (cd "data-collectors/$DC_NAME" && mvn -B -U clean compile test)
+    cd "data-collectors/$DC_NAME"
+    if test -n "$DRYRUN"; then
+        echo "DRYRUN: data-collectors/$DC_NAME"
+    else
+        echo
+        echo "!!! CHECKING $DC_NAME !!!"
+        echo
+        mvn -B -U clean compile test
+    fi
+    cd -
 done
+
 echo
 echo "!!! Ready !!!"
 echo
