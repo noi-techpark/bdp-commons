ec2bfbeb2e5741d012496ab11d3351dd9e359804
Peter Moser <p.moser@noi.bz.it>
2021-12-09 18:33:15 +0100

    environment-a22: fix test deployment

--- data-collectors/environment-a22/infrastructure/Jenkinsfile-Test.groovy
+++ data-collectors/environment-a22/infrastructure/Jenkinsfile-Test.groovy
@@ -1,14 +1,20 @@
 pipeline {
     agent any
-    
+
     environment {
-        PROJECT = "environment-a22"
-        PROJECT_FOLDER = "data-collectors/${PROJECT}"
-        ARTIFACT_NAME = "dc-${PROJECT}"
-        DOCKER_IMAGE = '755952719952.dkr.ecr.eu-west-1.amazonaws.com/dc-environment-a22'
-        DOCKER_TAG = "test-$BUILD_NUMBER"
-        DATACOLLECTORS_CLIENT_SECRET = credentials('keycloak-datacollectors-secret')
-        A22_CONNECTOR = credentials('a22connector_credentialsbn3')
+		LIMIT = "test"
+        ARTIFACT_NAME = "dc-environment-a22"
+        PROJECT_FOLDER = "data-collectors/${ARTIFACT_NAME}"
+        DOCKER_IMAGE = "755952719952.dkr.ecr.eu-west-1.amazonaws.com/${ARTIFACT_NAME}"
+        DOCKER_TAG = "$LIMIT-$BUILD_NUMBER"
+        DATACOLLECTORS_CLIENT_SECRET = credentials("keycloak-datacollectors-secret-${LIMIT}")
+        KEYCLOAK_URL = "https://auth.opendatahub.testingmachine.eu"
+        WRITER_URL = "https://mobility.share.opendatahub.testingmachine.eu"
+        LOG_LEVEL = "info"
+        A22_MQTT_USERNAME = credentials('a22-mqtt-username')
+        A22_MQTT_PASSWORD = credentials('a22-mqtt-password')
+        A22_MQTT_URI = credentials('a22-mqtt-uri')
+        A22_MQTT_PORT = credentials('a22-mqtt-port')
     }
 
     stages {
@@ -16,24 +22,28 @@ pipeline {
             steps {
                 sh """
                     cd ${PROJECT_FOLDER}
-                    echo 'COMPOSE_PROJECT_NAME=${PROJECT}' > .env
+                    echo 'COMPOSE_PROJECT_NAME=${ARTIFACT_NAME}' > .env
                     echo 'DOCKER_IMAGE=${DOCKER_IMAGE}' >> .env
                     echo 'DOCKER_TAG=${DOCKER_TAG}' >> .env
-                    echo 'LOG_LEVEL=debug' >> .env
+                    echo 'LOG_LEVEL=${LOG_LEVEL}' >> .env
                     echo 'ARTIFACT_NAME=${ARTIFACT_NAME}' >> .env
-                    echo 'authorizationUri=https://auth.opendatahub.testingmachine.eu/auth' >> .env
-                    echo 'tokenUri=https://auth.opendatahub.testingmachine.eu/auth/realms/noi/protocol/openid-connect/token' >> .env 
+                    echo 'authorizationUri=${KEYCLOAK_URL}/auth' >> .env
+                    echo 'tokenUri=${KEYCLOAK_URL}/auth/realms/noi/protocol/openid-connect/token' >> .env
                     echo 'clientId=odh-mobility-datacollector' >> .env
                     echo 'clientName=odh-mobility-datacollector' >> .env
                     echo 'clientSecret=${DATACOLLECTORS_CLIENT_SECRET}' >> .env
-                    echo 'scope=openid' >> .env 
+                    echo 'scope=openid' >> .env
                     echo -n 'provenance_version=' >> .env
                     xmlstarlet sel -N pom=http://maven.apache.org/POM/4.0.0 -t -v '/pom:project/pom:version' pom.xml >> .env
                     echo '' >> .env
-                    echo -n 'provenance_name=' >> .env 
+                    echo -n 'provenance_name=' >> .env
                     xmlstarlet sel -N pom=http://maven.apache.org/POM/4.0.0 -t -v '/pom:project/pom:artifactId' pom.xml >> .env
                     echo '' >> .env
-                    echo 'BASE_URI=https://share.opendatahub.testingmachine.eu/json' >> .env
+                    echo 'BASE_URI=${WRITER_URL}/json' >> .env
+                    echo 'A22_MQTT_USERNAME=${A22_MQTT_USERNAME}' >> .env
+                    echo 'A22_MQTT_PASSWORD=${A22_MQTT_PASSWORD}' >> .env
+                    echo 'A22_MQTT_URI=${A22_MQTT_URI}' >> .env
+                    echo 'A22_MQTT_PORT=${A22_MQTT_PORT}' >> .env
                 """
             }
         }
@@ -51,8 +61,9 @@ pipeline {
             steps {
                sshagent(['jenkins-ssh-key']) {
                     sh """
-                        (cd ${PROJECT_FOLDER}/infrastructure/ansible && ansible-galaxy install -f -r requirements.yml)
-                        (cd ${PROJECT_FOLDER}/infrastructure/ansible && ansible-playbook --limit=test deploy.yml --extra-vars "release_name=${BUILD_NUMBER} project_name=${PROJECT}")
+                        cd ${PROJECT_FOLDER}/infrastructure/ansible
+                        ansible-galaxy install -f -r requirements.yml
+                        ansible-playbook --limit=test deploy.yml --extra-vars "release_name=${BUILD_NUMBER} project_name=${ARTIFACT_NAME}"
                     """
                 }
             }
