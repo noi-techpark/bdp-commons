a01b8e1ff691d6a74654a8e5646fa6c223d0e03b
Peter Moser <p.moser@noi.bz.it>
2021-12-10 19:59:31 +0100

    environment-a22: update README

--- data-collectors/environment-a22/infrastructure/Jenkinsfile-Prod.groovy
+++ data-collectors/environment-a22/infrastructure/Jenkinsfile-Prod.groovy
@@ -1,22 +1,21 @@
 pipeline {
-    agent {
-        dockerfile {
-            filename 'data-collectors/environment-a22/infrastructure/docker/Dockerfile'
-        }
-    }
+    agent any
 
     environment {
+        LIMIT = "prod"
         PROJECT = "environment-a22"
         PROJECT_FOLDER = "data-collectors/${PROJECT}"
         ARTIFACT_NAME = "dc-${PROJECT}"
-        DOCKER_IMAGE = '755952719952.dkr.ecr.eu-west-1.amazonaws.com/dc-environment-a22'
-        DOCKER_TAG = "prod-$BUILD_NUMBER"
-        DATACOLLECTORS_CLIENT_SECRET = credentials('keycloak-datacollectors-secret-prod')
-        A22_MQTT_USERNAME=credentials('a22-mqtt-username')
-        A22_MQTT_PASSWORD=credentials('a22-mqtt-password')
-        A22_MQTT_PORT=credentials('a22-mqtt-port')
-        A22_MQTT_URI=credentials('a22-mqtt-uri')
-
+        DOCKER_IMAGE = "755952719952.dkr.ecr.eu-west-1.amazonaws.com/${ARTIFACT_NAME}"
+        DOCKER_TAG = "$LIMIT-$BUILD_NUMBER"
+        DATACOLLECTORS_CLIENT_SECRET = credentials("keycloak-datacollectors-secret-${LIMIT}")
+        KEYCLOAK_URL = "https://auth.opendatahub.bz.it"
+        WRITER_URL = "https://mobility.share.opendatahub.bz.it"
+        LOG_LEVEL = "info"
+        MQTT_USERNAME = credentials('a22-mqtt-username')
+        MQTT_PASSWORD = credentials('a22-mqtt-password')
+        MQTT_URI = credentials('a22-mqtt-uri')
+        MQTT_PORT = credentials('a22-mqtt-port')
     }
 
     stages {
@@ -24,13 +23,13 @@ pipeline {
             steps {
                 sh """
                     cd ${PROJECT_FOLDER}
-                    echo 'COMPOSE_PROJECT_NAME=${PROJECT}' > .env
+                    echo 'COMPOSE_PROJECT_NAME=${ARTIFACT_NAME}' > .env
                     echo 'DOCKER_IMAGE=${DOCKER_IMAGE}' >> .env
                     echo 'DOCKER_TAG=${DOCKER_TAG}' >> .env
-                    echo 'LOG_LEVEL=info' >> .env
+                    echo 'LOG_LEVEL=${LOG_LEVEL}' >> .env
                     echo 'ARTIFACT_NAME=${ARTIFACT_NAME}' >> .env
-                    echo 'authorizationUri=https://auth.opendatahub.bz.it/auth' >> .env
-                    echo 'tokenUri=https://auth.opendatahub.bz.it/auth/realms/noi/protocol/openid-connect/token' >> .env
+                    echo 'authorizationUri=${KEYCLOAK_URL}/auth' >> .env
+                    echo 'tokenUri=${KEYCLOAK_URL}/auth/realms/noi/protocol/openid-connect/token' >> .env
                     echo 'clientId=odh-mobility-datacollector' >> .env
                     echo 'clientName=odh-mobility-datacollector' >> .env
                     echo 'clientSecret=${DATACOLLECTORS_CLIENT_SECRET}' >> .env
@@ -41,30 +40,33 @@ pipeline {
                     echo -n 'provenance_name=' >> .env
                     xmlstarlet sel -N pom=http://maven.apache.org/POM/4.0.0 -t -v '/pom:project/pom:artifactId' pom.xml >> .env
                     echo '' >> .env
-                    echo 'BASE_URI=https://mobility.share.opendatahub.bz.it/json' >> .env
-                    echo 'MQTT_USERNAME=${A22_MQTT_USERNAME}' >> .env
-                    echo 'MQTT_PASSWORD=${A22_MQTT_PASSWORD}' >> .env
-                    echo 'MQTT_PORT=${A22_MQTT_PORT}' >> .env
-                    echo 'MQTT_URI=${A22_MQTT_URI}' >> .env
+                    echo 'BASE_URI=${WRITER_URL}/json' >> .env
+                    echo 'MQTT_USERNAME=${MQTT_USERNAME}' >> .env
+                    echo 'MQTT_PASSWORD=${MQTT_PASSWORD}' >> .env
+                    echo 'MQTT_URI=${MQTT_URI}' >> .env
+                    echo 'MQTT_PORT=${MQTT_PORT}' >> .env
                 """
             }
         }
         stage('Test & Build') {
             steps {
-                sh 'cd ${PROJECT_FOLDER} && mvn -B -U clean compile test'
+                sh """
+                    cd ${PROJECT_FOLDER}
+                    aws ecr get-login --region eu-west-1 --no-include-email | bash
+                    docker-compose --no-ansi -f infrastructure/docker-compose.build.yml build --pull
+                    docker-compose --no-ansi -f infrastructure/docker-compose.build.yml push
+                """
             }
         }
-        stage('Build') {
+        stage('Deploy') {
             steps {
-                sh 'cd ${PROJECT_FOLDER} && mvn package'
+               sshagent(['jenkins-ssh-key']) {
+                    sh """
+                        cd ${PROJECT_FOLDER}/infrastructure/ansible
+                        ansible-galaxy install -f -r requirements.yml
+                        ansible-playbook --limit=test deploy.yml --extra-vars "release_name=${BUILD_NUMBER} project_name=${ARTIFACT_NAME}"
+                    """
             }
-        }
-        stage('Archive') {
-            steps {
-                sh 'cp ${PROJECT_FOLDER}/target/${ARTIFACT_NAME}.war ${ARTIFACT_NAME}.war'
-                sh 'cp ${PROJECT_FOLDER}/.env ./'
-                sh 'zip ${ARTIFACT_NAME}.zip .env ${ARTIFACT_NAME}.war'
-                archiveArtifacts artifacts: "${ARTIFACT_NAME}.zip", onlyIfSuccessful: true
             }
         }
     }
