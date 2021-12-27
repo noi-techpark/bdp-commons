e3042a9f309cbb49551e9f781561165c3b16185f
Simon Dalvai <s.dalvai@noi.bz.it>
2021-12-27 15:32:12 +0100

    parking-MeBo: change prod hostname, set max heapsize to 512m and update log4j to 2.17.0

--- data-collectors/parking-MeBo/infrastructure/Jenkinsfile-Test.groovy
+++ data-collectors/parking-MeBo/infrastructure/Jenkinsfile-Test.groovy
@@ -9,6 +9,7 @@ pipeline {
         DOCKER_IMAGE = '755952719952.dkr.ecr.eu-west-1.amazonaws.com/dc-parking-mebo'
         DOCKER_TAG = "test-$BUILD_NUMBER"
         DATACOLLECTORS_CLIENT_SECRET = credentials('keycloak-datacollectors-secret')
+        JAVA_OPTIONS = "-Xms128m -Xmx512m"
     }
 
     stages {
@@ -44,6 +45,7 @@ pipeline {
                     xmlstarlet sel -N pom=http://maven.apache.org/POM/4.0.0 -t -v '/pom:project/pom:artifactId' pom.xml >> .env
                     echo '' >> .env
                     echo 'BASE_URI=https://share.opendatahub.testingmachine.eu/json' >> .env
+                    echo 'JAVA_OPTIONS=${JAVA_OPTIONS}' >> .env
                 """
             }
         }
