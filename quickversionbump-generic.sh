023a34fca69f53d886f97d14f49472f92e7d8c77
Simon Dalvai <s.dalvai@noi.bz.it>
2022-01-03 14:30:12 +0100

    fix #52: create xmlstarlet script to update dependencies

---
+++ quickversionbump-generic.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Will update all pom.xml dependencies of this folder, means upgrade the given dependency for all data-collectors
+#
+
+if [ ! $# -eq 3 ]
+  then
+    echo "3 arguments needed: groupId artifactId version"
+    echo "example to update log4j-core to 2.17.1:"
+    echo "./quickversionbump-generic.sh org.apache.logging.log4j log4j-core 2.17.1"
+    exit 1
+fi
+
+set -euo pipefail
+
+GROUP_ID="$1"
+ARTIFACT_ID="$2"
+VERSION="$3"
+XMLNS=http://maven.apache.org/POM/4.0.0
+
+find ./ -name pom.xml -exec sh -c "xmlstarlet ed -P -L -N pom=$XMLNS -u '/pom:project/pom:dependencies/pom:dependency[pom:groupId=\"$GROUP_ID\"][pom:artifactId=\"$ARTIFACT_ID\"]/pom:version' -v \"$VERSION\" {}" \;
+echo "$GROUP_ID $ARTIFACT_ID  updated to $VERSION" 
+
+exit 0
