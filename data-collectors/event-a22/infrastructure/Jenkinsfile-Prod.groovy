a5a16543344100b3e8bf4aa3acd6f91400320d54
Peter Moser <p.moser@noi.bz.it>
2022-02-08 17:55:10 +0100

    event-a22: configure production pipeline

--- data-collectors/event-a22/infrastructure/Jenkinsfile-Prod.groovy
+++ data-collectors/event-a22/infrastructure/Jenkinsfile-Prod.groovy
@@ -6,8 +6,8 @@ pipeline {
         LOG_LEVEL = "info"
         PROJECT = "event-a22"
         PROJECT_FOLDER = "data-collectors/${PROJECT}"
-        ARTIFACT_NAME = "dc-${PROJECT}"
-        DOCKER_IMAGE = "755952719952.dkr.ecr.eu-west-1.amazonaws.com/${PROJECT}"
+        ARTIFACT_NAME = "odh-mobility-dc-${PROJECT}"
+        DOCKER_IMAGE = "755952719952.dkr.ecr.eu-west-1.amazonaws.com/${ARTIFACT_NAME}"
         DOCKER_TAG = "$LIMIT-$BUILD_NUMBER"
         DATACOLLECTORS_CLIENT_SECRET = credentials("keycloak-datacollectors-secret-$LIMIT")
         KEYCLOAK_URL = "https://auth.opendatahub.testingmachine.eu"
@@ -23,21 +23,21 @@ pipeline {
             steps {
                 sh """
                     cd ${PROJECT_FOLDER}
-                    echo 'COMPOSE_PROJECT_NAME=${PROJECT}' > .env
+                    echo 'COMPOSE_PROJECT_NAME=${ARTIFACT_NAME}' > .env
                     echo 'DOCKER_IMAGE=${DOCKER_IMAGE}' >> .env
                     echo 'DOCKER_TAG=${DOCKER_TAG}' >> .env
                     echo 'LOG_LEVEL=${LOG_LEVEL}' >> .env
                     echo 'ARTIFACT_NAME=${ARTIFACT_NAME}' >> .env
                     echo 'authorizationUri=${KEYCLOAK_URL}/auth' >> .env
-                    echo 'tokenUri=${KEYCLOAK_URL}/auth/realms/noi/protocol/openid-connect/token' >> .env 
+                    echo 'tokenUri=${KEYCLOAK_URL}/auth/realms/noi/protocol/openid-connect/token' >> .env
                     echo 'clientId=odh-mobility-datacollector' >> .env
                     echo 'clientName=odh-mobility-datacollector' >> .env
                     echo 'clientSecret=${DATACOLLECTORS_CLIENT_SECRET}' >> .env
-                    echo 'scope=openid' >> .env 
+                    echo 'scope=openid' >> .env
                     echo -n 'provenance_version=' >> .env
                     xmlstarlet sel -N pom=http://maven.apache.org/POM/4.0.0 -t -v '/pom:project/pom:version' pom.xml >> .env
                     echo '' >> .env
-                    echo -n 'provenance_name=' >> .env 
+                    echo -n 'provenance_name=' >> .env
                     xmlstarlet sel -N pom=http://maven.apache.org/POM/4.0.0 -t -v '/pom:project/pom:artifactId' pom.xml >> .env
                     echo '' >> .env
                     echo 'BASE_URI=${WRITER_URL}/json' >> .env
@@ -62,8 +62,9 @@ pipeline {
             steps {
                sshagent(['jenkins-ssh-key']) {
                     sh """
-                        (cd ${PROJECT_FOLDER}/infrastructure/ansible && ansible-galaxy install -f -r requirements.yml)
-                        (cd ${PROJECT_FOLDER}/infrastructure/ansible && ansible-playbook --limit=${LIMIT} deploy.yml --extra-vars "release_name=${BUILD_NUMBER} project_name=${PROJECT}")
+                        cd ${PROJECT_FOLDER}/infrastructure/ansible
+                        ansible-galaxy install -f -r requirements.yml
+                        ansible-playbook --limit=${LIMIT} deploy.yml --extra-vars "release_name=${BUILD_NUMBER} project_name=${ARTIFACT_NAME}"
                     """
                 }
             }
