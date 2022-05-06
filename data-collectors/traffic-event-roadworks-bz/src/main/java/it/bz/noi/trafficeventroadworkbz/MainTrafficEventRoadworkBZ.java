5eaa48a068aa33b7602cd77a0ea13f9c402303ff
Peter Moser <p.moser@noi.bz.it>
2022-05-06 15:24:28 +0200

    traffic-event-roadworks-bz: use new keycloak credentials

--- data-collectors/traffic-event-roadworks-bz/src/main/java/it/bz/noi/trafficeventroadworkbz/MainTrafficEventRoadworkBZ.java
+++ data-collectors/traffic-event-roadworks-bz/src/main/java/it/bz/noi/trafficeventroadworkbz/MainTrafficEventRoadworkBZ.java
@@ -13,8 +13,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.time.Instant;
-import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
