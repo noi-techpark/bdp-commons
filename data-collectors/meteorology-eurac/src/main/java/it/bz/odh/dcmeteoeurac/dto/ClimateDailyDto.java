72933c75bf88a5fb1734aa547fa72b9dc6ef0cfd
Saimonas Vaičkus <93773535+SaimonasFOS@users.noreply.github.com>
2022-07-19 18:36:03 +0300

    Simplify climate daily request, update documentation

--- data-collectors/meteorology-eurac/src/main/java/it/bz/odh/dcmeteoeurac/dto/ClimateDailyDto.java
+++ data-collectors/meteorology-eurac/src/main/java/it/bz/odh/dcmeteoeurac/dto/ClimateDailyDto.java
@@ -15,16 +15,6 @@ public class ClimateDailyDto {
     
     private Double prec;
     
-    private int id;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public Double getTmin() {
         return tmin;
     }
