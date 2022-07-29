4be93c46b95a6cb268fdb7e21efe65ee6012977a
Thomas Nocker <thomas.nocker@catch-solve.tech>
2022-07-29 08:41:40 +0200

    data collector car pooling init commit

---
+++ data-collectors/car-pooling/src/main/java/it/bz/odh/carpooling/CsvFileUtilities.java
@@ -0,0 +1,101 @@
+package it.bz.odh.carpooling;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.io.input.BOMInputStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvFileUtilities {
+
+	private static final Logger LOG = LoggerFactory.getLogger(CsvFileUtilities.class);
+
+	public static List<CarPoolingTripDto> parseCarPoolingCsvData(InputStream inputStream) {
+		LOG.info("read car pooling csv data");
+
+		List<CarPoolingTripDto> tripsList = new ArrayList<>();
+
+		try {
+			LOG.info("parse par pooling trips csv file from url");
+			BOMInputStream bomInputStream = new BOMInputStream(inputStream);
+			CSVParser csvParser = CSVParser.parse(bomInputStream,
+				Charset.forName(bomInputStream.hasBOM()? bomInputStream.getBOMCharsetName(): "UTF-8"),
+				CSVFormat.RFC4180.withFirstRecordAsHeader());
+			int parseSuccess = 0;
+			int parseFailed = 0;
+			for (CSVRecord csvRecord : csvParser) {
+				CarPoolingTripDto carPoolingTrip = new CarPoolingTripDto();
+				try {
+					carPoolingTrip.setHashedId(csvRecord.get("hashed_id"));
+					carPoolingTrip.setStatus(csvRecord.get("status"));
+					carPoolingTrip.setRideStartAtUtc(extractZonedDateTime(csvRecord, "ride_start_at_UTC"));
+					carPoolingTrip.setRideDistanceKm(extractDouble(csvRecord, "ride_distance_km"));
+					carPoolingTrip.setRideDurationMinute(extractDouble(csvRecord, "ride_duration_minutes"));
+					carPoolingTrip.setRideCreatedAt(extractZonedDateTime(csvRecord, "ride_created_at_UTC"));
+					carPoolingTrip.setRideModifiedAt(extractZonedDateTime(csvRecord, "ride_modified_at_UTC"));
+					carPoolingTrip.setSeatsReserved(extractInteger(csvRecord, "seats_reserved"));
+					carPoolingTrip.setStartLatApprox(extractDouble(csvRecord, "start_lat_approx"));
+					carPoolingTrip.setStartLonApprox(extractDouble(csvRecord, "start_lon_approx"));
+					carPoolingTrip.setStartPostCode(csvRecord.get("start_post_code"));
+					carPoolingTrip.setEndLatApprox(extractDouble(csvRecord, "end_lat_approx"));
+					carPoolingTrip.setEndLonApprox(extractDouble(csvRecord, "end_lon_approx"));
+					carPoolingTrip.setEndPostCode(csvRecord.get("end_post_code"));
+
+					tripsList.add(carPoolingTrip);
+					System.out.println(carPoolingTrip.toJson());
+					parseSuccess++;
+				} catch (Exception e) {
+					LOG.warn("parsing csv record to car pooling trip failed: " + e.getMessage(), e);
+					parseFailed++;
+				}
+			}
+			LOG.info("got {} csv records, {} successfully parsed, {} failed to parse", parseSuccess + parseFailed, parseSuccess, parseFailed);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		return tripsList;
+	}
+
+	private static ZonedDateTime extractZonedDateTime(CSVRecord csvRecord, String csvFieldName) {
+		String csvFieldValue = csvRecord.get(csvFieldName);
+		if(csvFieldValue == null || csvFieldValue.isEmpty())
+			return null;
+		return LocalDateTime.parse(csvRecord.get(csvFieldName), DateTimeFormatter.ofPattern("M/d/yyyy h:mm:ss a")).atZone(ZoneOffset.UTC);
+	}
+
+	private static Integer extractInteger(CSVRecord csvRecord, String csvFieldName) {
+		String csvFieldValue = csvRecord.get(csvFieldName);
+		if(csvFieldValue == null || csvFieldValue.isEmpty())
+			return null;
+		try {
+			return Integer.parseInt(csvFieldValue);
+		} catch (NumberFormatException e) {
+			LOG.error("Unable to parse the following value of the csv field '{}' to integer: {}", csvFieldName, csvFieldValue);
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static Double extractDouble(CSVRecord csvRecord, String csvFieldName) {
+		String csvFieldValue = csvRecord.get(csvFieldName);
+		if(csvFieldValue == null || csvFieldValue.isEmpty())
+			return null;
+		try {
+			return Double.parseDouble(csvFieldValue);
+		} catch (NumberFormatException e) {
+			LOG.error("Unable to parse the following value of the csv field '{}' to double: {}", csvFieldName, csvFieldValue);
+			throw new RuntimeException(e);
+		}
+	}
+}
---
+++ data-collectors/car-pooling/src/main/java/it/bz/odh/carpooling/SyncScheduler.java
@@ -0,0 +1,112 @@
+package it.bz.odh.carpooling;
+
+import it.bz.idm.bdp.dto.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class SyncScheduler {
+
+	private static final Logger LOG = LoggerFactory.getLogger(SyncScheduler.class);
+	private static final String DATATYPE_ITINERARY_ID = "itinerary_details";
+
+	@Value("${odh_client.period}")
+    private Integer period;
+
+    @Lazy
+    @Autowired
+    private OdhClient odhClient;
+
+	@Autowired
+	private GoogleDriveConnector googleDriveConnector;
+
+	@Scheduled(cron = "${scheduler.job_car_pooling}")
+	public void carPoolingMain() {
+		LOG.info("Cron car pooling man started");
+		initDataTypes();
+
+		long currentTimeInMillis = System.currentTimeMillis();
+
+		List<CarPoolingTripDto> carPoolingTripList = CsvFileUtilities.parseCarPoolingCsvData(googleDriveConnector.readRidesCsvContent());
+
+		StationList stationList = new StationList();
+		DataMapDto<RecordDtoImpl> dataMap = new DataMapDto<>();
+
+		LOG.info("got {} car pooling trips", carPoolingTripList.size());
+
+		for(int i = 0; i < carPoolingTripList.size(); i++) {
+			CarPoolingTripDto carPoolingTrip = carPoolingTripList.get(i);
+			StationDto stationDto = new StationDto(carPoolingTrip.getHashedId(),
+				"-",
+				carPoolingTrip.getStartLatApprox(),
+				carPoolingTrip.getStartLonApprox());
+
+			stationDto.setOrigin(odhClient.getProvenance().getLineage());
+
+			stationDto.getMetaData().put("start_lat_approx", carPoolingTrip.getStartLatApprox());
+			stationDto.getMetaData().put("start_lon_approx", carPoolingTrip.getStartLonApprox());
+			stationDto.getMetaData().put("end_lat_approx", carPoolingTrip.getEndLatApprox());
+			stationDto.getMetaData().put("end_lon_approx", carPoolingTrip.getEndLonApprox());
+
+			stationList.add(stationDto);
+
+			dataMap.addRecord(stationDto.getId(), DATATYPE_ITINERARY_ID,
+				new SimpleRecordDto(currentTimeInMillis, carPoolingTrip.toJson(), period));
+		}
+
+
+		Map<String, String> externalStation = new HashMap<>();
+		externalStation.put("name", "Cool Measurement Station 1");
+		externalStation.put("lat", "46.4861067");
+		externalStation.put("lon", "11.3275207");
+
+		LOG.info("sync {} car pooling stations", stationList.size());
+		try {
+			odhClient.syncStations(stationList);
+		} catch (WebClientRequestException e) {
+			LOG.error("Sync stations failed: Request exception: {}", e.getMessage());
+		}
+
+		LOG.info("push trip data about {} car pooling stations", stationList.size());
+		try {
+			odhClient.pushData(dataMap);
+		} catch (WebClientRequestException e) {
+			LOG.error("Sync stations failed: Request exception: {}", e.getMessage());
+		}
+	}
+
+	private void initDataTypes() {
+		LOG.info("init car pooling data types");
+		Map<String, String> externalDataType = new HashMap<>();
+		externalDataType.put("name", DATATYPE_ITINERARY_ID);
+		externalDataType.put("unit", "json");
+
+		List<DataTypeDto> odhDataTypeList = new ArrayList<>();
+		odhDataTypeList.add(
+			new DataTypeDto(
+				externalDataType.get("name"),
+				externalDataType.get("unit"),
+				externalDataType.get("name"),
+				"Instantaneous"
+			)
+		);
+
+		try {
+			odhClient.syncDataTypes(odhDataTypeList);
+		} catch (WebClientRequestException e) {
+			LOG.error("Sync data types failed: Request exception: {}", e.getMessage());
+		}
+	}
+
+}
