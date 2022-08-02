53246a0fb609616233e35923499cf83d29c0d5c9
Peter Schweigkofler <p.schweigkofler080202@gmail.com>
2022-08-02 16:44:38 +0200

    create logic to sync traffic stations

--- data-collectors/traffic-provBZ/src/main/java/it/bz/odh/trafficprovbz/SyncScheduler.java
+++ data-collectors/traffic-provBZ/src/main/java/it/bz/odh/trafficprovbz/SyncScheduler.java
@@ -1,13 +1,13 @@
 package it.bz.odh.trafficprovbz;
 
-import org.slf4j.LoggerFactory;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import com.jayway.jsonpath.JsonPath;
+import it.bz.idm.bdp.dto.*;
+import it.bz.odh.trafficprovbz.dto.AggregatedDataDto;
+import it.bz.odh.trafficprovbz.dto.ClassificationSchemaDto;
+import it.bz.odh.trafficprovbz.dto.MetadataDto;
+import net.minidev.json.JSONObject;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
@@ -15,124 +15,134 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClientRequestException;
 
-import it.bz.idm.bdp.dto.DataMapDto;
-import it.bz.idm.bdp.dto.DataTypeDto;
-import it.bz.idm.bdp.dto.RecordDtoImpl;
-import it.bz.idm.bdp.dto.SimpleRecordDto;
-import it.bz.idm.bdp.dto.StationDto;
-import it.bz.idm.bdp.dto.StationList;
+import java.io.IOException;
+import java.util.*;
 
 @Service
 public class SyncScheduler {
 
 	private static final Logger LOG = LoggerFactory.getLogger(SyncScheduler.class);
 
-    private static final String STATION_ID = "some-unique-id-1234";
-	private static final String DATATYPE_ID = "Temperature";
+	private static final String STATION_TYPE = "TrafficSensor";
+	private static final String DATATYPE_ID = "TrafficSensor";
 
 	@Value("${odh_client.period}")
-    private Integer period;
+	private Integer period;
 
-    @Lazy
-    @Autowired
-    private OdhClient odhClient;
+	@Lazy
+	@Autowired
+	private OdhClient odhClient;
 
-    /**
-     * Scheduled job A: Example to sync stations and data types
-     */
-    @Scheduled(cron = "${scheduler.job_a}")
-    public void syncJobA() {
-        LOG.info("Cron job A started: Sync Stations with type {} and data types", odhClient.getIntegreenTypology());
+	@Lazy
+	@Autowired
+	private FamasClient famasClient;
 
-		// 1) Retrieve your station data from some endpoint (this is a mock)
-		Map<String, String> externalStation = new HashMap<>();
-		externalStation.put("name", "Cool Measurement Station");
-		externalStation.put("lat", "46.4861067");
-		externalStation.put("lon", "11.3275207");
-
-		Map<String, String> externalDataType = new HashMap<>();
-		externalDataType.put("name", DATATYPE_ID);
-		externalDataType.put("unit", "°C");
-
-		// 2) Parse results and fill a list of StationDto and DataTypeDto objects
-		StationDto station = new StationDto(
-			STATION_ID,
-			externalStation.get("name"),
-			Double.parseDouble(externalStation.get("lat")),
-			Double.parseDouble(externalStation.get("lon"))
-		);
-		// best practice: set also an origin, so that we understand where this station came from
-		station.setOrigin(odhClient.getProvenance().getLineage());
-		// Use station.getMetaData().put(.....) if you want to add additional information
-		StationList odhStationList = new StationList();
-		odhStationList.add(station);
+	/**
+	 * Scheduled job stations: Sync stations and data types
+	 */
+	@Scheduled(cron = "${scheduler.job_stations}")
+	public void syncJobStations() throws IOException {
+		LOG.info("Cron job stations started: Sync Stations with type {} and data types", odhClient.getIntegreenTypology());
 
 		List<DataTypeDto> odhDataTypeList = new ArrayList<>();
-		odhDataTypeList.add(
-			new DataTypeDto(
-				externalDataType.get("name"),
-				externalDataType.get("unit"),
-				"temperature in celsius from blablabla...",
-				"max"  // explaining the metric type (min, max, mean, etc.)
-			)
-			// Use .getMetaData().put(.....) if you want to add additional information
-		);
 
-		// 3) Send it to the Open Data Hub INBOUND API (writer)
+		ClassificationSchemaDto[] classificationDtos = famasClient.getClassificationSchemas();
+		ArrayList<LinkedHashMap<String, String>> odhClassesList = new ArrayList<>();
+
+		for (ClassificationSchemaDto c : classificationDtos) {
+			ArrayList<LinkedHashMap<String, String>> classes = JsonPath.read(c.getOtherFields(), "$.Classi");
+			odhClassesList.addAll(classes);
+		}
+
+
+		MetadataDto[] metadataDtos = famasClient.getStationsData();
+		StationList odhStationList = new StationList();
+
+		for (MetadataDto metadataDto : metadataDtos) {
+			JSONObject otherFields = new JSONObject(metadataDto.getOtherFields());
+			Double lat = JsonPath.read(otherFields, "$.GeoInfo.Latitudine");
+			Double lon = JsonPath.read(otherFields, "$.GeoInfo.Longitudine");
+
+			LinkedHashMap<String, String> classificationSchema = getClassificationSchema(odhClassesList, metadataDto);
+			metadataDto.setOtherField("SchemaDiClassificazione", classificationSchema);
+
+			ArrayList<LinkedHashMap<String, String>> lanes = JsonPath.read(otherFields, "$.CorsieInfo");
+			LOG.info(String.valueOf(lanes.get(0)));
+
+			for (LinkedHashMap<String, String> lane : lanes) {
+				String description = JsonPath.read(lane, "$.Descrizione");
+				String stationName = metadataDto.getName() + ":" + description;
+				StationDto station = new StationDto(String.valueOf(metadataDto.getId()), stationName, lat, lon);
+				station.setOrigin(odhClient.getProvenance().getLineage());
+				station.setStationType(STATION_TYPE);
+				station.setMetaData(metadataDto.getOtherFields());
+
+				odhStationList.add(station);
+			}
+			LOG.info(odhStationList.toString());
+		}
+
 		try {
 			odhClient.syncStations(odhStationList);
 			odhClient.syncDataTypes(odhDataTypeList);
-			LOG.info("Cron job A successful");
+			LOG.info("Cron job stations successful");
 		} catch (WebClientRequestException e) {
-			LOG.error("Cron job A failed: Request exception: {}", e.getMessage());
+			LOG.error("Cron job stations failed: Request exception: {}", e.getMessage());
 		}
-    }
+	}
 
 	/**
-     * Scheduled job B: Example on how to send measurements
-     */
-    @Scheduled(cron = "${scheduler.job_b}")
-    public void syncJobB() {
-        LOG.info("Cron job B started: Pushing measurements for {}", odhClient.getIntegreenTypology());
+	 * Scheduled job measurements: Example on how to send measurements
+	 */
+	@Scheduled(cron = "${scheduler.job_measurements}")
+	public void syncJobMeasurements() throws IOException {
+		LOG.info("Cron job measurements started: Pushing measurements for {}", odhClient.getIntegreenTypology());
 
-		// 1) Retrieve your measurement data from some endpoint (this is a mock)
-		//   a) The measurement itself (including a measurement timestamp)
-		long externalTimestamp = 1651741173000L;  // we use unix epoch timestamps (in millis) here
-		Double externalMeasurement = -12.3;
-		//   b) Associate the measurement somehow to an existing station and data type
-		String odhStationName = STATION_ID;
-		String odhDataTypeName = DATATYPE_ID;
-
-		// 2) Build a data map which has the following form:
-		//		(root) >> Station >> DataType >> Measurements
-		//
-		//    The data map is generic, therefore we need to create single
-		//    branches starting from a root pointer.
-		//
-		// We show each step separately for faster understanding:
 		DataMapDto<RecordDtoImpl> rootMap = new DataMapDto<>();
-		DataMapDto<RecordDtoImpl> stationMap = rootMap.upsertBranch(odhStationName);
-		DataMapDto<RecordDtoImpl> metricMap = stationMap.upsertBranch(odhDataTypeName);
-		SimpleRecordDto measurement = new SimpleRecordDto(externalTimestamp, externalMeasurement);
-		// The period is an interval between one measurement and the consecutive one (seconds)
-		// It tells us, how often something gets measured.
-		// For example, every 10 minutes = every 600 seconds
-		// Since this is the same for all measurements, and should be configurable we use a property value
-		measurement.setPeriod(period);
-		List<RecordDtoImpl> values = metricMap.getData();
-		values.add(measurement);
 
-		// 3) Send the measurements to the Open Data Hub INBOUND API (writer)
-		//    WARNING: stations and datatypes must already exist, otherwise this call will fail
-		//             It does not throw any exception, it will just not insert that data (this is a known issue)
-		//             Exception will only be thrown on connection errors here! Please refer to the
-		//             writer log output or the database itself to see if data has been inserted
+		AggregatedDataDto[] aggregatedDataDtos = famasClient.getAggregatedDataOnStations();
+
+		for (AggregatedDataDto aggregatedDataDto : aggregatedDataDtos) {
+
+			DataMapDto<RecordDtoImpl> stationMap = rootMap.upsertBranch(String.valueOf(aggregatedDataDto.getId()));
+			DataMapDto<RecordDtoImpl> metricMap = stationMap.upsertBranch(DATATYPE_ID);
+			Map<String, Object> aggregatedDataMap = new HashMap<>();
+			aggregatedDataMap.put("total-transits", aggregatedDataDto.getTotalTransits());
+			//TODO: LOGIC FOR BIKES AND SO ON
+			JSONObject otherFields = new JSONObject(aggregatedDataDto.getOtherFields());
+			if (otherFields.containsKey("TotaliPerClasseVeicolare")) {
+				System.out.println("HERER");
+			} else {
+				System.out.println("NOT ALWAYS HERER");
+
+			}
+			aggregatedDataMap.put("average-vehicle-speed", aggregatedDataDto.getAverageVehicleSpeed());
+			aggregatedDataMap.put("headway", aggregatedDataDto.getHeadway());
+			aggregatedDataMap.put("headway-variance", aggregatedDataDto.getHeadwayVariance());
+			aggregatedDataMap.put("gap", aggregatedDataDto.getGap());
+			aggregatedDataMap.put("gap-variance", aggregatedDataDto.getGapVariance());
+
+
+			SimpleRecordDto measurement = new SimpleRecordDto(22323L, aggregatedDataMap, period);
+			List<RecordDtoImpl> values = metricMap.getData();
+			values.add(measurement);
+		}
 		try {
 			odhClient.pushData(rootMap);
-			LOG.info("Cron job B successful");
+			LOG.info("Cron job measurements successful");
 		} catch (WebClientRequestException e) {
-			LOG.error("Cron job B failed: Request exception: {}", e.getMessage());
+			LOG.error("Cron job measurements failed: Request exception: {}", e.getMessage());
 		}
-    }
+	}
+
+	public LinkedHashMap<String, String> getClassificationSchema(ArrayList<LinkedHashMap<String, String>> odhClassesList, MetadataDto s) {
+		for (LinkedHashMap<String, String> odhClass : odhClassesList) {
+			int code = JsonPath.read(odhClass, "$.Codice");
+			if (code == s.getClassificationSchema()) {
+				return odhClass;
+			}
+		}
+		return null;
+	}
 
 }
