61a5dc7262a2412b406f3d676349bca43666e49a
Peter Schweigkofler <p.schweigkofler080202@gmail.com>
2022-08-03 14:24:23 +0200

    write unit tests to check if application doesn´t crash if errors occur

---
+++ data-collectors/traffic-provBZ/src/test/java/it/bz/odh/trafficprovbz/SyncSchedulerTest.java
@@ -0,0 +1,53 @@
+package it.bz.odh.trafficprovbz;
+
+import it.bz.idm.bdp.dto.StationList;
+import it.bz.odh.trafficprovbz.dto.ClassificationSchemaDto;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+
+class SyncSchedulerTest {
+
+	//@Mock
+	FamasClient famasClient = Mockito.mock(FamasClient.class);
+
+	//@Mock
+	OdhClient odhClient = Mockito.mock(OdhClient.class);
+
+	@Test
+	void test() {
+		assertThat(true).isEqualTo(true);
+	}
+
+	@Test
+	void checkSyncStations() throws Exception {
+		Mockito.when(famasClient.getClassificationSchemas()).thenReturn(null);
+		Mockito.when(famasClient.getStationsData()).thenReturn(null);
+
+		SyncScheduler sut = new SyncScheduler(odhClient, famasClient);
+
+		sut.syncJobStations();
+	}
+
+	@Test
+	void checkSyncTrafficMeasurements() throws Exception {
+		Mockito.when(famasClient.getAggregatedDataOnStations()).thenReturn(null);
+
+		SyncScheduler sut = new SyncScheduler(odhClient, famasClient);
+
+		sut.syncJobTrafficMeasurements();
+	}
+
+	@Test
+	void checkSyncBluetoothMeasurements() throws Exception {
+		int STATION_ID = 1;
+		Mockito.when(famasClient.getPassagesDataOnStations(STATION_ID)).thenReturn(null);
+
+		SyncScheduler sut = new SyncScheduler(odhClient, famasClient);
+
+		sut.syncJobBluetoothMeasurements();
+	}
+}
