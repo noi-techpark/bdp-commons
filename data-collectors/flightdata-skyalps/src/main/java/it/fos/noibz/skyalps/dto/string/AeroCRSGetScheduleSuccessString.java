539e1d13ecd4b457c0fd7c2ba8381f92f9de3d7f
Simon Dalvai <s.dalvai@noi.bz.it>
2022-12-01 11:50:04 +0100

    flightdata-skyalps: catch and debug log exception on flight decoding

--- data-collectors/flightdata-skyalps/src/main/java/it/fos/noibz/skyalps/dto/string/AeroCRSGetScheduleSuccessString.java
+++ data-collectors/flightdata-skyalps/src/main/java/it/fos/noibz/skyalps/dto/string/AeroCRSGetScheduleSuccessString.java
@@ -11,6 +11,9 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import it.fos.noibz.skyalps.dto.json.AeroCRSFlight;
@@ -24,6 +27,8 @@ import net.logstash.logback.encoder.org.apache.commons.lang3.StringUtils;
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class AeroCRSGetScheduleSuccessString {
+
+	private static final Logger LOG = LoggerFactory.getLogger(AeroCRSGetScheduleSuccessString.class);
 
 	private boolean success;
 	private String flight;
@@ -116,97 +121,101 @@ public class AeroCRSGetScheduleSuccessString {
 			// related to flights, and it will retrieve strings with the same lengths
 			if (flightArray[i].contains(" ") && flightSSIMLenght == SSIMSTRINGLENGHT
 					&& flightArray[i] != flightIgnore) {
-				aeroFlight.setWeekdaymon(false);
-				aeroFlight.setWeekdaytue(false);
-				aeroFlight.setWeekdaywed(false);
-				aeroFlight.setWeekdaythu(false);
-				aeroFlight.setWeekdayfri(false);
-				aeroFlight.setWeekdaysat(false);
-				aeroFlight.setWeekdaysun(false);
+				try {
+					aeroFlight.setWeekdaymon(false);
+					aeroFlight.setWeekdaytue(false);
+					aeroFlight.setWeekdaywed(false);
+					aeroFlight.setWeekdaythu(false);
+					aeroFlight.setWeekdayfri(false);
+					aeroFlight.setWeekdaysat(false);
+					aeroFlight.setWeekdaysun(false);
 
-				String fltnumber = flightArray[i].substring(FLTNUMBERSTART, FLTNUMBEREND);
-				String monthfromComplete = flightArray[i].substring(MONTHFROM, MONTHFROMFINAL);
-				String monthToComplete = flightArray[i].substring(MONTHTO, MONTHTOFINAL);
-				String fromDestination = flightArray[i].substring(FROMDESTINATIONSTART, FROMDESTINATIONEND);
-				String toDestination = flightArray[i].substring(TODESTINATIONSTART, TODESTINATIONEND);
-				String airlineID = flightArray[i].substring(AIRLINEIDSTART, AIRLINEIDEND);
-				String accode = flightArray[i].substring(ACCODESTART, ACCODEND);
-				String yearFromComplete = flightArray[i].substring(YEAFROMSTART, YEARFROMEND);
-				String yearToComplete = flightArray[i].substring(YEARTOSTART, YEARTOEND);
-				String dayFromComplete = flightArray[i].substring(DAYFROMSTART, DAYFROMEND);
-				String dayToComplete = flightArray[i].substring(DAYTOSTART, DAYTOEND);
-				String stdHour = flightArray[i].substring(STDHOURSTART, STDHOUREND);
-				String stdMinute = flightArray[i].substring(STDMINUTESTART, STDMINUTEEND);
-				String staHour = flightArray[i].substring(STAHOURSTART, STAHOUREND);
-				String staMinute = flightArray[i].substring(STAMINUTESTART, STAMINUTEND);
+					String fltnumber = flightArray[i].substring(FLTNUMBERSTART, FLTNUMBEREND);
+					String monthfromComplete = flightArray[i].substring(MONTHFROM, MONTHFROMFINAL);
+					String monthToComplete = flightArray[i].substring(MONTHTO, MONTHTOFINAL);
+					String fromDestination = flightArray[i].substring(FROMDESTINATIONSTART, FROMDESTINATIONEND);
+					String toDestination = flightArray[i].substring(TODESTINATIONSTART, TODESTINATIONEND);
+					String airlineID = flightArray[i].substring(AIRLINEIDSTART, AIRLINEIDEND);
+					String accode = flightArray[i].substring(ACCODESTART, ACCODEND);
+					String yearFromComplete = flightArray[i].substring(YEAFROMSTART, YEARFROMEND);
+					String yearToComplete = flightArray[i].substring(YEARTOSTART, YEARTOEND);
+					String dayFromComplete = flightArray[i].substring(DAYFROMSTART, DAYFROMEND);
+					String dayToComplete = flightArray[i].substring(DAYTOSTART, DAYTOEND);
+					String stdHour = flightArray[i].substring(STDHOURSTART, STDHOUREND);
+					String stdMinute = flightArray[i].substring(STDMINUTESTART, STDMINUTEEND);
+					String staHour = flightArray[i].substring(STAHOURSTART, STAHOUREND);
+					String staMinute = flightArray[i].substring(STAMINUTESTART, STAMINUTEND);
 
-				TemporalAccessor accessorFrom = parser
-						.parse(flightArray[i].charAt(MONTHFROMFIRST) + monthfromComplete.toLowerCase());
-				TemporalAccessor accessorTo = parser
-						.parse(flightArray[i].charAt(MONTHTOFIST) + monthToComplete.toLowerCase());
+					TemporalAccessor accessorFrom = parser
+							.parse(flightArray[i].charAt(MONTHFROMFIRST) + monthfromComplete.toLowerCase());
+					TemporalAccessor accessorTo = parser
+							.parse(flightArray[i].charAt(MONTHTOFIST) + monthToComplete.toLowerCase());
 
-				int yearFrom = YEAR + Integer.parseInt(yearFromComplete);
-				int yearTo = YEAR + Integer.parseInt(yearToComplete);
-				int monthFrom = accessorFrom.get(ChronoField.MONTH_OF_YEAR);
-				int monthTo = accessorTo.get(ChronoField.MONTH_OF_YEAR);
-				int dayFrom = Integer.parseInt(dayFromComplete);
-				int dayTo = Integer.parseInt(dayToComplete);
-				Calendar calFrom = Calendar.getInstance();
-				calFrom.set(Calendar.YEAR, yearFrom);
-				calFrom.set(Calendar.MONTH, monthFrom);
-				calFrom.set(Calendar.DAY_OF_MONTH, dayFrom);
-				Calendar calTo = Calendar.getInstance();
-				calTo.set(Calendar.YEAR, yearTo);
-				calTo.set(Calendar.MONTH, monthTo);
-				calTo.set(Calendar.DAY_OF_MONTH, dayTo);
+					int yearFrom = YEAR + Integer.parseInt(yearFromComplete);
+					int yearTo = YEAR + Integer.parseInt(yearToComplete);
+					int monthFrom = accessorFrom.get(ChronoField.MONTH_OF_YEAR);
+					int monthTo = accessorTo.get(ChronoField.MONTH_OF_YEAR);
+					int dayFrom = Integer.parseInt(dayFromComplete);
+					int dayTo = Integer.parseInt(dayToComplete);
+					Calendar calFrom = Calendar.getInstance();
+					calFrom.set(Calendar.YEAR, yearFrom);
+					calFrom.set(Calendar.MONTH, monthFrom);
+					calFrom.set(Calendar.DAY_OF_MONTH, dayFrom);
+					Calendar calTo = Calendar.getInstance();
+					calTo.set(Calendar.YEAR, yearTo);
+					calTo.set(Calendar.MONTH, monthTo);
+					calTo.set(Calendar.DAY_OF_MONTH, dayTo);
 
-				Calendar caSTD = Calendar.getInstance();
-				Calendar caSTA = Calendar.getInstance();
-				Date paredDateSTD = DFSTDA
-						.parse(DFFROMTO.format(calFrom.getTime()) + " " + stdHour + ":" + stdMinute);
-				caSTD.setTime(paredDateSTD);
-				Date paredDateSTA = DFSTDA
-						.parse(DFFROMTO.format(calTo.getTime()) + " " + staHour + ":" + staMinute);
-				SimpleDateFormat dfs = new SimpleDateFormat("HH:mm");
-				caSTA.setTime(paredDateSTA);
+					Calendar caSTD = Calendar.getInstance();
+					Calendar caSTA = Calendar.getInstance();
+					Date paredDateSTD = DFSTDA
+							.parse(DFFROMTO.format(calFrom.getTime()) + " " + stdHour + ":" + stdMinute);
+					caSTD.setTime(paredDateSTD);
+					Date paredDateSTA = DFSTDA
+							.parse(DFFROMTO.format(calTo.getTime()) + " " + staHour + ":" + staMinute);
+					SimpleDateFormat dfs = new SimpleDateFormat("HH:mm");
+					caSTA.setTime(paredDateSTA);
 
-				aeroFlight.setFltnumber(StringUtils.deleteWhitespace(fltnumber));
-				aeroFlight.setAirlinename(AIRLINENAME);
-				aeroFlight.setFromdestination(fromDestination);
-				aeroFlight.setTodestination(toDestination);
-				aeroFlight.setAirlineid(airlineID);
-				aeroFlight.setAccode(accode);
-				aeroFlight.setFltsfromperiod(DFFROMTO.format(calFrom.getTime()));
-				aeroFlight.setFltstoperiod(DFFROMTO.format(calTo.getTime()));
-				aeroFlight.setStd(dfs.format(caSTD.getTime()));
-				aeroFlight.setSta(dfs.format(caSTA.getTime()));
-				aeroFlight.setDateStart(dayFromComplete+monthfromComplete+yearFromComplete);
-				aeroFlight.setDateEnd(dayToComplete+monthToComplete+yearToComplete);
-				aeroFlight.setSsimMessage(flightArray[i]);
-				
-				if (String.valueOf(flightArray[i].charAt(MONDAYCHAR)).equals(MONDAY)) {
-					aeroFlight.setWeekdaymon(true);
-				}
-				if (String.valueOf(flightArray[i].charAt(TUESDTACHAR)).equals(TUESDAY)) {
-					aeroFlight.setWeekdaytue(true);
-				}
-				if (String.valueOf(flightArray[i].charAt(WEDCHAR)).equals(WEDNESDAY)) {
-					aeroFlight.setWeekdaywed(true);
-				}
-				if (String.valueOf(flightArray[i].charAt(THUCHAR)).equals(THURSDAY)) {
-					aeroFlight.setWeekdaythu(true);
-				}
-				if (String.valueOf(flightArray[i].charAt(FRICHAR)).equals(FRIDAY)) {
-					aeroFlight.setWeekdayfri(true);
-				}
-				if (String.valueOf(flightArray[i].charAt(SATCHAR)).equals(SATURDAY)) {
-					aeroFlight.setWeekdaysat(true);
-				}
-				if (String.valueOf(flightArray[i].charAt(SUNCHAR)).equals(SUNDAY)) {
-					aeroFlight.setWeekdaysun(true);
-				}
+					aeroFlight.setFltnumber(StringUtils.deleteWhitespace(fltnumber));
+					aeroFlight.setAirlinename(AIRLINENAME);
+					aeroFlight.setFromdestination(fromDestination);
+					aeroFlight.setTodestination(toDestination);
+					aeroFlight.setAirlineid(airlineID);
+					aeroFlight.setAccode(accode);
+					aeroFlight.setFltsfromperiod(DFFROMTO.format(calFrom.getTime()));
+					aeroFlight.setFltstoperiod(DFFROMTO.format(calTo.getTime()));
+					aeroFlight.setStd(dfs.format(caSTD.getTime()));
+					aeroFlight.setSta(dfs.format(caSTA.getTime()));
+					aeroFlight.setDateStart(dayFromComplete + monthfromComplete + yearFromComplete);
+					aeroFlight.setDateEnd(dayToComplete + monthToComplete + yearToComplete);
+					aeroFlight.setSsimMessage(flightArray[i]);
 
-				flightsList.add(aeroFlight);
+					if (String.valueOf(flightArray[i].charAt(MONDAYCHAR)).equals(MONDAY)) {
+						aeroFlight.setWeekdaymon(true);
+					}
+					if (String.valueOf(flightArray[i].charAt(TUESDTACHAR)).equals(TUESDAY)) {
+						aeroFlight.setWeekdaytue(true);
+					}
+					if (String.valueOf(flightArray[i].charAt(WEDCHAR)).equals(WEDNESDAY)) {
+						aeroFlight.setWeekdaywed(true);
+					}
+					if (String.valueOf(flightArray[i].charAt(THUCHAR)).equals(THURSDAY)) {
+						aeroFlight.setWeekdaythu(true);
+					}
+					if (String.valueOf(flightArray[i].charAt(FRICHAR)).equals(FRIDAY)) {
+						aeroFlight.setWeekdayfri(true);
+					}
+					if (String.valueOf(flightArray[i].charAt(SATCHAR)).equals(SATURDAY)) {
+						aeroFlight.setWeekdaysat(true);
+					}
+					if (String.valueOf(flightArray[i].charAt(SUNCHAR)).equals(SUNDAY)) {
+						aeroFlight.setWeekdaysun(true);
+					}
+
+					flightsList.add(aeroFlight);
+				} catch (Exception e) {
+					LOG.debug("error while parsing line {}", flightArray[i]);
+				}
 			}
 		}
 		flights.setSuccess(true);
