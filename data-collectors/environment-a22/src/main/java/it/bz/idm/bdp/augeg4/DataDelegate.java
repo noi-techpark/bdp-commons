cb1ee7c35e3f1433b4e0ff71104244fd1ba53f5a
Clemens Zagler <c.zagler@noi.bz.it>
2023-06-21 08:47:45 +0200

    env-a22: disable push of processed data to AUGE, as processing formulas and parameters are out of date

--- data-collectors/environment-a22/src/main/java/it/bz/idm/bdp/augeg4/DataDelegate.java
+++ data-collectors/environment-a22/src/main/java/it/bz/idm/bdp/augeg4/DataDelegate.java
@@ -103,7 +103,7 @@ public class DataDelegate {
      */
     public void pushData() {
         fetchData();
-        pushDataToAuge();
+        //pushDataToAuge();
         pushDataToHub();
     }
 
