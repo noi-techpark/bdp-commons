acc0c2562b47763ffd0fa9ce82df2236ff96f420
Clemens Zagler <c.zagler@noi.bz.it>
2023-08-09 09:12:18 +0200

    bikeboxes: update datatype IDs and descriptions

--- data-collectors/bike-boxes/src/main/java/com/opendatahub/bdp/commons/dc/bikeboxes/BikeBoxJobScheduler.java
+++ data-collectors/bike-boxes/src/main/java/com/opendatahub/bdp/commons/dc/bikeboxes/BikeBoxJobScheduler.java
@@ -92,9 +92,9 @@ public class BikeBoxJobScheduler {
 				// create station level measurements (as key value pairs)
 				var stationData = Map.of(
 						DataTypes.usageState.key, mapBikeStationBayState(bs.state),
-						DataTypes.availableMuscularBikes.key, bs.countFreePlacesAvailable_MuscularBikes,
-						DataTypes.availableAssistedBikes.key, bs.countFreePlacesAvailable_AssistedBikes,
-						DataTypes.availableVehicles.key, bs.countFreePlacesAvailable);
+						DataTypes.freeSpotsNonEbikes.key, bs.countFreePlacesAvailable_MuscularBikes,
+						DataTypes.freeSpotsEbikes.key, bs.countFreePlacesAvailable_AssistedBikes,
+						DataTypes.free.key, bs.countFreePlacesAvailable);
 				// add the created measurements to odh data list
 				stationData.forEach((t, v) -> odhData.addRecord(stationDto.getId(), t, mapSimple(v)));
 
@@ -124,6 +124,7 @@ public class BikeBoxJobScheduler {
 			LOG.debug("Pushing data to ODH");
 			odhClient.syncDataTypes(stationC.stationBayType,
 					Arrays.stream(DataTypes.values())
+							.filter(d -> d.syncToOdh)
 							.map(DataTypes::toDataTypeDto)
 							.toList());
 			odhClient.syncStations(stationC.stationType, odhStations, 25);
--- data-collectors/bike-boxes/src/main/java/com/opendatahub/bdp/commons/dc/bikeboxes/config/DataTypes.java
+++ data-collectors/bike-boxes/src/main/java/com/opendatahub/bdp/commons/dc/bikeboxes/config/DataTypes.java
@@ -11,18 +11,20 @@ import it.bz.idm.bdp.dto.DataTypeDto;
 
 public enum DataTypes {
     /** FREE, OCCUPIED, OUT OF SERVICE */
-    usageState("usageState", "usageState"),
-    availableMuscularBikes("availableMuscularBikes", "availableMuscularBikes"),
-    availableAssistedBikes("availableAssistedBikes", "availableAssistedBikes"),
-    availableVehicles("availableVehicles", "availableVehicles");
+    usageState("usageState", true, "Usage state"),
+    freeSpotsRegularBikes("freeSpotsRegularBike", true, "Free parking spots (regular bikes)"),
+    freeSpotsElectricBikes("freeSpotsElectricBike", true, "Free parking spots (electric bikes)"),
+    free("free", false, "Free parking spots");
 
-    private DataTypes(String key, String description) {
+    private DataTypes(String key, boolean sync, String description) {
         this.key = key;
         this.description = description;
+        this.syncToOdh = sync;
     }
 
+    public final boolean syncToOdh;
     public final String key;
-    public final String unit = "";
+    public final String unit = "count";
     public final String description;
     public final String rtype = "Instantaneous";
 
