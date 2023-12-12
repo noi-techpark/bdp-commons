6cd8bc9b0ded0db1e2962c6ab7f44a2354e2dc1c
Simon Dalvai <s.dalvai@noi.bz.it>
2023-12-12 16:29:38 +0100

    meteorology-tn: fix main package

--- data-collectors/meteorology-tn/src/main/java/it/bz/idm/bdp/dcmeteotn/Main.java
+++ data-collectors/meteorology-tn/src/main/java/it/bz/idm/bdp/dcmeteotn/Main.java
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-package it.bz.idm.bdp;
+package it.bz.idm.bdp.dcmeteotn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
