26d0212f1984f532f788351e0dd49fc8f4b38040
Simon Dalvai <s.dalvai@noi.bz.it>
2024-02-14 12:36:52 +0100

    parking-offstreet-sta: save data for all types to ParkingFacility

--- data-collectors/parking-offstreet-sta/src/dc/job.go
+++ data-collectors/parking-offstreet-sta/src/dc/job.go
@@ -16,13 +16,13 @@ const stationTypeParent string = "ParkingFacility"
 const stationType string = "ParkingStation"
 
 const shortStay string = "short_stay"
-const Subscribers string = "subscribers"
+const subscribers string = "subscribers"
 
 const dataTypeFreeShort string = "free_" + shortStay
-const dataTypeFreeSubs string = "free_" + Subscribers
+const dataTypeFreeSubs string = "free_" + subscribers
 const dataTypeFreeTotal string = "free"
 const dataTypeOccupiedShort string = "occupied_" + shortStay
-const dataTypeOccupiedSubs string = "occupied_" + Subscribers
+const dataTypeOccupiedSubs string = "occupied_" + subscribers
 const dataTypeOccupiedTotal string = "occupied"
 
 var origin string = os.Getenv("ORIGIN")
@@ -67,6 +67,14 @@ func Job() {
 			freeTotalSum := 0
 			occupiedTotalSum := 0
 			capacityTotal := 0
+			// total facility subscribers measurements
+			freeSubscribersSum := 0
+			occupiedSubscribersSum := 0
+			capacitySubscribers := 0
+			// total facility short stay measurements
+			freeShortStaySum := 0
+			occupiedShortStaySum := 0
+			capacityShortStay := 0
 
 			// freeplaces is array of a single categories data
 			// if multiple parkNo exist, multiple entries for every parkNo and its categories exist
@@ -91,18 +99,26 @@ func Job() {
 					station.MetaData["capacity_"+shortStay] = freePlace.Capacity
 					bdplib.AddRecord(stationCode, dataTypeFreeShort, bdplib.CreateRecord(ts, freePlace.FreePlaces, 600), &dataMap)
 					bdplib.AddRecord(stationCode, dataTypeOccupiedShort, bdplib.CreateRecord(ts, freePlace.CurrentLevel, 600), &dataMap)
+					// facility data
+					freeShortStaySum += freePlace.FreePlaces
+					occupiedShortStaySum += freePlace.CurrentLevel
+					capacityShortStay += freePlace.Capacity
 				// Subscribed
 				case 2:
-					station.MetaData["free_limit_"+Subscribers] = freePlace.FreeLimit
-					station.MetaData["occupancy_limit_"+Subscribers] = freePlace.OccupancyLimit
-					station.MetaData["capacity_"+Subscribers] = freePlace.Capacity
+					station.MetaData["free_limit_"+subscribers] = freePlace.FreeLimit
+					station.MetaData["occupancy_limit_"+subscribers] = freePlace.OccupancyLimit
+					station.MetaData["capacity_"+subscribers] = freePlace.Capacity
 					bdplib.AddRecord(stationCode, dataTypeFreeSubs, bdplib.CreateRecord(ts, freePlace.FreePlaces, 600), &dataMap)
 					bdplib.AddRecord(stationCode, dataTypeOccupiedSubs, bdplib.CreateRecord(ts, freePlace.CurrentLevel, 600), &dataMap)
+					// facility data
+					freeSubscribersSum += freePlace.FreePlaces
+					occupiedSubscribersSum += freePlace.CurrentLevel
+					capacitySubscribers += freePlace.Capacity
 				// Total
 				default:
 					station.MetaData["free_limit"] = freePlace.FreeLimit
 					station.MetaData["occupancy_limit"] = freePlace.OccupancyLimit
-					station.MetaData["Capacity"] = freePlace.Capacity
+					station.MetaData["capacity"] = freePlace.Capacity
 					bdplib.AddRecord(stationCode, dataTypeFreeTotal, bdplib.CreateRecord(ts, freePlace.FreePlaces, 600), &dataMap)
 					bdplib.AddRecord(stationCode, dataTypeOccupiedTotal, bdplib.CreateRecord(ts, freePlace.CurrentLevel, 600), &dataMap)
 					// total facility data
@@ -111,6 +127,7 @@ func Job() {
 					capacityTotal += freePlace.Capacity
 				}
 			}
+
 			// assign total facility data, if data is not 0
 			if freeTotalSum > 0 {
 				bdplib.AddRecord(parentStationCode, dataTypeFreeTotal, bdplib.CreateRecord(ts, freeTotalSum, 600), &dataMapParent)
@@ -119,7 +136,29 @@ func Job() {
 				bdplib.AddRecord(parentStationCode, dataTypeOccupiedTotal, bdplib.CreateRecord(ts, occupiedTotalSum, 600), &dataMapParent)
 			}
 			if capacityTotal > 0 {
-				parentStation.MetaData["Capacity"] = capacityTotal
+				parentStation.MetaData["capacity"] = capacityTotal
+			}
+
+			// subscribers
+			if freeSubscribersSum > 0 {
+				bdplib.AddRecord(parentStationCode, dataTypeFreeSubs, bdplib.CreateRecord(ts, freeSubscribersSum, 600), &dataMapParent)
+			}
+			if occupiedSubscribersSum > 0 {
+				bdplib.AddRecord(parentStationCode, dataTypeOccupiedSubs, bdplib.CreateRecord(ts, occupiedTotalSum, 600), &dataMapParent)
+			}
+			if capacitySubscribers > 0 {
+				parentStation.MetaData["capacity_"+subscribers] = capacityTotal
+			}
+
+			// short stay
+			if freeShortStaySum > 0 {
+				bdplib.AddRecord(parentStationCode, dataTypeFreeShort, bdplib.CreateRecord(ts, freeShortStaySum, 600), &dataMapParent)
+			}
+			if occupiedShortStaySum > 0 {
+				bdplib.AddRecord(parentStationCode, dataTypeOccupiedShort, bdplib.CreateRecord(ts, occupiedShortStaySum, 600), &dataMapParent)
+			}
+			if capacityShortStay > 0 {
+				parentStation.MetaData["capacity_"+shortStay] = capacityTotal
 			}
 		}
 	}
