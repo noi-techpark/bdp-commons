c9e7fa5ced5dd77cf16852c588b25d1634d76c71
Simon Dalvai <s.dalvai@noi.bz.it>
2024-06-03 11:39:36 +0200

    radelt: fix reuse

--- data-collectors/radelt/src/main/java/com/opendatahub/bdp/radelt/dto/organisationen/RadeltStatisticsDto.java
+++ data-collectors/radelt/src/main/java/com/opendatahub/bdp/radelt/dto/organisationen/RadeltStatisticsDto.java
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 package com.opendatahub.bdp.radelt.dto.organisationen;
 
 import java.util.List;
