97b814327042176dd01a12fdaba6c4e0e246fa53
Simon Dalvai <s.dalvai@noi.bz.it>
2024-06-11 11:23:28 +0200

    environment-appa-bz-opendata: simplify timestamp conversion | 2

--- data-collectors/environment-appa-bz-opendata/src/main/java/Helpers/DateHelper.java
+++ data-collectors/environment-appa-bz-opendata/src/main/java/Helpers/DateHelper.java
@@ -9,10 +9,13 @@ import org.slf4j.Logger;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+
+
 public class DateHelper {
 
     private static final Logger LOG = LoggerFactory.getLogger(DateHelper.class.getName());
-	private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-ddTHH:mm:ssXXX");
+	private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
     /**
      * This method, given the input parameters, produces a timeStamp used
      * for the measurements mapping.
@@ -22,7 +25,11 @@ public class DateHelper {
     {
         LOG.debug("Producing timestamp...");
         try {
-            return dateFormat.parse(input).getTime();
+			input = input.replace("\"", "");
+			LOG.debug("date: {}", input);
+			LOG.debug("instant: {}", dateFormat.parse(input).getTime());
+			return dateFormat.parse(input).getTime();
+			//return instant.toEpochMilli();
         } catch (ParseException e)
         {
             LOG.error("Impossible to parse time and set timestamp.");
