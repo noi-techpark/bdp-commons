100b24c6a1248abb0a41cc228e14d4bf6360ad33
Clemens Zagler <c.zagler@noi.bz.it>
2025-03-07 15:13:10 +0100

    remove data collector spreadsheet google, because migrated. Remove defunct config for umadumm that never went into production

--- data-collectors/spreadsheets-google/src/main/java/it/bz/odh/spreadsheets/ISpreadsheetCollector.java
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
-//
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-package it.bz.odh.spreadsheets;
-
-public interface ISpreadsheetCollector {
-    public void syncData();
-}
